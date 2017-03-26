2eb1ac250bf33c7d37e512fb60e4bcecafd32717
CHIp Wright <frisbeeprod@msn.com>
2017-03-26 13:04:04 -0700

    please work

--- Practice/udemy-react/8.9.routing-nav/app/components/About.jsx
+++ Practice/udemy-react/8.9.routing-nav/app/components/About.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
-//
+
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>welcome to the about page, again.</p>
+            <h3>About Component</h3>
+            <p>welcome to the about page, finally</p>
+            <p></p>
         </div>
     )
 };
