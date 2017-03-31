79baf4497a1843a34c4449690b327295c0990ec5
CHIp Wright <frisbeeprod@msn.com>
2017-03-30 19:54:04 -0700

    edit

---
+++ Practice/udemy-react/8.routing-styling/8.13.modal-for-errors/app/components/About.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+//
+var About = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather app built on react!</p>
+            <p>
+                Here are some of the tools I used:
+            </p>
+            <ul>
+                <li>
+                    <a href="http://facebook.github.io/react" target="_blank">React</a> - Framework used
+                </li>
+                <li>
+                    <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - Used open weather map to search for weather data
+                </li>
+            </ul>
+        </div>
+    )
+};
+
+module.exports = About;
