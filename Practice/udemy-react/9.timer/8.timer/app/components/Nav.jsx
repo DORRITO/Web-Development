2053b5bb2dbcaaf32362c1c38d90c0b041eb6d7f
CHIp Wright <frisbeeprod@msn.com>
2017-04-05 03:56:59 -0700

    initial commit

---
+++ Practice/udemy-react/9.timer/8.timer/app/components/Nav.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var {NavLink} = require('react-router-dom');
+
+var Nav = () => {
+
+        return(
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Time App</li>
+                        <li>
+                            <NavLink to="/timer" activeClassName="active">Timer</NavLink></li>
+                        <li>
+                            <NavLink to="/countdown" activeClassName="active">Countdown</NavLink>
+                        </li>
+                   </ul>
+                </div>
+                <div className="top-bar-right">
+                   <ul className="menu">
+                       <li className="menu-text">Created by <a>CHIp</a></li>
+                   </ul>
+                </div>
+            </div>
+        )
+    
+};
+
+module.exports = Nav;
