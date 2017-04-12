86f9147fc550b4438fba7c905640881ed0497c66
CHIp Wright <frisbeeprod@msn.com>
2017-04-12 05:28:42 -0700

    addTodo

--- Practice/udemy-react/10.todo/4.wiring-addTodo/app/components/TodoApp.jsx
+++ Practice/udemy-react/10.todo/4.wiring-addTodo/app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('uuid');
 
 var TodoApp = React.createClass({
     
@@ -12,19 +13,19 @@ var TodoApp = React.createClass({
             searchText: '',
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: 'Walk the dog'
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: 'Clean the yard'
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: 'Mow the lawn'
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: 'Do the dishes'
                 },
             ]
@@ -32,7 +33,15 @@ var TodoApp = React.createClass({
     },
     
     handleAddTodo: function(text) {
-        alert('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        });
     },
     
     handleSearch: function(showCompleted, searchText) {
