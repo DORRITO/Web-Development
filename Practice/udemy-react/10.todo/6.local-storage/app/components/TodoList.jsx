0303d9dd3bb0d1377402349702b13b6c8efd473c
CHIp Wright <frisbeeprod@msn.com>
2017-04-13 03:45:51 -0700

    used local storage for the first time!

---
+++ Practice/udemy-react/10.todo/6.local-storage/app/components/TodoList.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+    
+    render: function() {
+        var {todos} = this.props;
+        
+        var renderTodos = () => {
+            return todos.map((todo) => {
+                return(
+                    //... passes down every attribute as props, cool!  key makes it so react can keep track of list items.
+                    <Todo key={todo.id} {...todo} onToggle={this.props.onToggle} />
+                )
+            });
+        };
+        
+        return(
+            <div>
+                {renderTodos()}
+            </div>
+        )
+    } 
+});
+
+module.exports = TodoList;
