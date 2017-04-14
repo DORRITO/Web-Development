766ae3f6ea866c3f985cc25bc2d78020c89c327e
CHIp Wright <frisbeeprod@msn.com>
2017-04-13 22:29:21 -0700

    cleaned up look

--- Practice/udemy-react/10.todo/7.1.wiring-search/app/api/TodoAPI.jsx
+++ Practice/udemy-react/10.todo/7.1.wiring-search/app/api/TodoAPI.jsx
@@ -29,14 +29,12 @@ module.exports = {
         //Filter showCompleted
         filteredTodos = filteredTodos.filter((todo) => {
             return !todo.completed || showCompleted;
-        });
-        
+        });       
         //Filter by searchText
         filteredTodos = filteredTodos.filter((todo) => {
            var text = todo.text.toLowerCase();
             return searchText.length === 0 || text.indexOf(searchText) > -1;
-        });
-        
+        });       
         //Sort todos with non-completed first
         filteredTodos.sort((a, b) => {
             if (!a.completed && b.completed) {
