35cf359f5b9ba00015c667540118a3bd5d017694
CHIp Wright <frisbeeprod@msn.com>
2017-04-14 03:01:42 -0700

    added todo times

--- Practice/udemy-react/10.todo/8.dates-using-moment/app/components/TodoApp.jsx
+++ Practice/udemy-react/10.todo/8.dates-using-moment/app/components/TodoApp.jsx
@@ -1,12 +1,11 @@
 var React = require('react');
 var uuid = require('uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
-
-
 
 var TodoApp = React.createClass({
     
@@ -14,7 +13,8 @@ var TodoApp = React.createClass({
         return {          
             showCompleted: false,
             searchText: '',
-            todos: TodoAPI.getTodos()           
+            todos: TodoAPI.getTodos(),
+            completedAt: undefined
         }; 
     },
     
@@ -29,7 +29,9 @@ var TodoApp = React.createClass({
                 {
                     id: uuid(),
                     text: text,
-                    completed: false
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
                 }
             ]
         });
@@ -39,6 +41,7 @@ var TodoApp = React.createClass({
         var updatedTodos = this.state.todos.map((todo) => {
             if (todo.id === id) {
                 todo.completed = !todo.completed;
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
             }
             
             return todo;
