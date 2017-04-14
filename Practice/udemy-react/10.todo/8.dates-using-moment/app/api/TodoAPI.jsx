d8619abff12425b22f09bd404f9f6401584ca7fc
CHIp Wright <frisbeeprod@msn.com>
2017-04-14 01:34:07 -0700

    initial commit

---
+++ Practice/udemy-react/10.todo/8.dates-using-moment/app/api/TodoAPI.jsx
@@ -0,0 +1,49 @@
+//jquery modules
+var $ = require('jquery');
+
+module.exports = {
+    setTodos: function(todos) {
+        if ($.isArray(todos)) {
+            localStorage.setItem('todos', JSON.stringify(todos));
+            return todos;
+        }
+    },
+    
+    getTodos: function() {
+        var stringTodos = localStorage.getItem('todos');
+        var todos = [];
+        
+        try {
+            todos = JSON.parse(stringTodos);
+        } catch(e){}
+        //ternary version of if
+        return $.isArray(todos) ? todos : []; 
+//        if ($.isArray(todos)) {
+//            return todos;
+//        } else {return [];}
+    },
+    
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+        
+        //Filter showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });       
+        //Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+           var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });       
+        //Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {return 0;}
+        });
+        
+        return filteredTodos;
+    }
+};
