e9134854f06d05edf79d901ccbe3c3b27ca1fd6d
CHIp Wright <frisbeeprod@msn.com>
2017-04-16 21:33:28 -0700

    file commit

--- Practice/udemy-meteor-react/score-keep/1.score-keep/client/main.js
+++
@@ -1,5 +0,0 @@
-import {greetUser, name} from './../imports/utils';
-
-console.log("Log from /clients/main.js");
-console.log(greetUser());
-console.log(name);
--- Practice/udemy-meteor-react/score-keep/1.score-keep/imports/utils.js
+++
@@ -1,7 +0,0 @@
-console.log('log from /imports/utils.js');
-
-export let greetUser = function(){
-    return 'hello user!';
-}
-
-export let name = 'chip';
