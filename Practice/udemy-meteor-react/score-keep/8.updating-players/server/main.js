482719e997dd3b3c2bcf2dd31f4b267b6ebf602f
CHIp Wright <frisbeeprod@msn.com>
2017-04-20 18:51:28 -0700

    added some class practice

--- Practice/udemy-meteor-react/score-keep/8.updating-players/server/main.js
+++ Practice/udemy-meteor-react/score-keep/8.updating-players/server/main.js
@@ -2,5 +2,13 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
+  class Person {
+    constructor(age = '0') {
+      this.age = age;
+    }
+    getGreeting() { return `hi, im ${this.age}` }
+  }
 
+  let me = new Person(2);
+  console.log(me.getGreeting());
 });
