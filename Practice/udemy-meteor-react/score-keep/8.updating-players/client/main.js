5fc837fe8beaa1414a629e83231ae7aa6e2bb485
CHIp Wright <frisbeeprod@msn.com>
2017-04-20 17:21:28 -0700

    added plus and minus functions

--- Practice/udemy-meteor-react/score-keep/8.updating-players/client/main.js
+++ Practice/udemy-meteor-react/score-keep/8.updating-players/client/main.js
@@ -10,7 +10,9 @@ const renderPlayers = (playersList) => {
     return(
       <p key={player._id}>
         {player.name} has {player.score} points
-        <button onClick={() => Players.remove({_id: player._id})}>delete</button>
+        <button onClick={() => {Players.update(player._id, {$inc: {score: 1}})}}>+1</button>
+        <button onClick={() => {Players.update(player._id, {$inc: {score: -1}})}}>-1</button>
+        <button onClick={() => Players.remove(player._id)}>delete</button>
       </p>
     );
   });
