c09ba889553faaf82896d9640cc6dee0add42877
CHIp Wright <frisbeeprod@msn.com>
2017-04-23 04:36:34 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/score-keep/14.conditional-rendering/imports/ui/PlayerList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+/////////////////////////////
+import Player from './Player'
+
+export default class PlayerList extends React.Component {
+  renderPlayers() {
+    if (this.props.players.length === 0) {
+      return <p>Please add a player!</p>
+    } else {
+      return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });
+    }
+  }
+  render() {
+    return(
+      <div>
+        {this.renderPlayers()}
+      </div>
+    );
+  }
+};
+
+PlayerList.PropTypes = {
+  players: PropTypes.array.isRequired
+}
