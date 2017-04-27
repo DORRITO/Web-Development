fc42ad88891bfa7567acf076017b64b6a030e186
CHIp Wright <frisbeeprod@msn.com>
2017-04-27 15:27:10 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/score-keep/21.3rd-party-components(flipmove)/imports/ui/Player.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Players} from './../api/players';
+
+export default class Player extends React.Component{
+  render() {
+      return(
+        <div key={this.props.player._id} className="item">
+          <div className="player">
+            <div>
+              <h3 className="player__name" >{this.props.player.name}</h3>
+              <p className="player__stats" >{this.props.player.score} points</p>
+            </div>
+            <div className="player__actions">
+              <button className="button button--round" onClick={() => {Players.update(this.props.player._id, {$inc: {score: 1}})}}>+1</button>
+              <button className="button button--round" onClick={() => {Players.update(this.props.player._id, {$inc: {score: -1}})}}>-1</button>
+              <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>delete</button>
+            </div>
+          </div>
+        </div>
+      );
+  }
+};
+//////////////////////////
+Player.propTypes = {
+  player: PropTypes.object.isRequired
+}
