16a0c90b685b7b2e6595c04a52f5e65fd9587c78
CHIp Wright <frisbeeprod@msn.com>
2017-05-01 00:01:38 -0700

    setstate

--- Practice/udemy-meteor-react/3.short-link/4.component-state&signup-form/imports/ui/Signup.js
+++ Practice/udemy-meteor-react/3.short-link/4.component-state&signup-form/imports/ui/Signup.js
@@ -2,13 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Signup extends React.Component{
+
+  //////set state/////////
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.count || 0
+    };
+  }///////////////////////
+
+  ////////error handler////
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      error: 'something went wrong.'
+    })
+  }/////////////////////////
+
+  //////button click function/////
+  increment() {
+    this.setState({
+      count: this.state.count +1,
+      error: ''
+    });
+  }////////////////////////////////
+
+  //////////render component////////
   render(){
     return(
       <div>
         <h1>Join short link</h1>
 
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <p>{this.state.count}</p>
+        <button onClick={this.increment.bind(this)}>+1</button>
+        <button onClick={() => this.setState({count: this.state.count -1})}>-1</button>{/*arrow function to decrease state number!*/}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" name="email" placeholder="email" />
+          <input type="password" name="password" placeholder="password" />
+          <button>Create Account</button>
+        </form>
+
         <Link to="/">Already have an account?</Link>
       </div>
     );
-  }
+  }/////////////////////////////////
 }
