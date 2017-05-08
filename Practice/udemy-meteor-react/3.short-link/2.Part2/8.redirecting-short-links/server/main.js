6be299a3af331217bf15409e6ad56877574c670a
CHIp Wright <frisbeeprod@msn.com>
2017-05-08 15:53:56 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/3.short-link/2.Part2/8.redirecting-short-links/server/main.js
@@ -0,0 +1,25 @@
+import { Meteor } from 'meteor/meteor';
+import {WebApp} from 'meteor/webapp';
+
+import '../imports/api/users';
+import '../imports/api/links';
+import '../imports/startup/simple-schema-configuration.js';
+
+Meteor.startup(() => {
+  //code to run on server at startup
+  WebApp.connectHandlers.use((req, res, next) => {
+    console.log('this is from my custom middleware');
+    console.log(req.url, req.method, req.headers, req.query);
+    //Set HTTP status code
+    res.statusCode = 404;
+    //set HTTP headers
+    res.setHeader('my-custom-header', 'Chip was here');
+    //Set HTTP body
+    res.write('<h1>This is my middleware at work</h1>');
+    //end HTTP request
+    //res.end();
+  });
+});
+//req: request comes in
+//res: run our middleware one at a time
+//next: send them that page
