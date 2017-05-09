13b4d7df192029da2dcd8661355579d1057642ee
CHIp Wright <frisbeeprod@msn.com>
2017-05-09 15:55:57 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/3.short-link/3.part3/2.stateless-components/imports/ui/LinksList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Links} from '../api/links';
+
+export default class LinksList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount linkslist');
+    this.linksTracker = Tracker.autorun(() => {
+      Meteor.subscribe('links'); //subscribes to the publish in links api, uses its name.
+      const links = Links.find().fetch();
+      this.setState({ links });
+    });
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount linkslist');
+    this.linksTracker.stop();
+  }
+
+  renderLinksListItems() {
+    return this.state.links.map((link) => {
+      return <p key={link._id}>{link.url}</p>
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <p>Links list</p>
+        <div>
+          {this.renderLinksListItems()}
+        </div>
+      </div>
+    )
+  }
+}
