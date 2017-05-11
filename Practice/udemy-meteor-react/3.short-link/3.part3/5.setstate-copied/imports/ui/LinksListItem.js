87f2c49520f8f50896b7a4dca8e43a74b3c1a199
CHIp Wright <frisbeeprod@msn.com>
2017-05-11 03:01:43 -0700

    copied/copy

--- Practice/udemy-meteor-react/3.short-link/3.part3/5.setstate-copied/imports/ui/LinksListItem.js
+++ Practice/udemy-meteor-react/3.short-link/3.part3/5.setstate-copied/imports/ui/LinksListItem.js
@@ -3,25 +3,41 @@ import Clipboard from 'clipboard';
 import PropTypes from 'prop-types';
 
 export default class LinksListItem extends React.Component{
+
+  ///////set state,switch to copied////
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }///////////////////////
+
+  ////////////clicking copy happened///////
   componentDidMount(){
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on('success', () => {
-      alert('it worked');
+      this.setState({ justCopied: true }); //changing state to true so 'copied' shows
+      setTimeout(() => this.setState({ justCopied: false }), 1000);//back to false a second late
     }).on('error', () => {
       alert('unable to copy, please manually copy link.');
     })
-  }
+  }/////////////////////////////////////
+
+  ///////////get rid of everything/////
   componentWillUnmount(){
     console.log('burrninating the countryside');
     this.clipboard.destroy();
-  }
+  }///////////////////////////////
+
   render(){
     return(
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          {this.state.justCopied ? 'copied' : 'copy'}
+        </button>
       </div>
     );
   }
