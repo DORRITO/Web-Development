e08f5afa02a1e2442240afbbd01ead280dbca4d9
CHIp Wright <frisbeeprod@msn.com>
2017-05-11 17:03:10 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/3.short-link/3.part3/7.meteor-session(showing-active-links)/client/main.js
@@ -0,0 +1,18 @@
+import {Meteor} from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import {Tracker} from 'meteor/tracker';
+
+import {routes, onAuthChange} from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration.js';
+
+//tracker is tracking anytime something in the database changes!
+Tracker.autorun(() => {
+  // takes a falsy or truthy value and makes it a boolean!  straight false or true.
+  // Meteor.userid is checking the id value
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.getElementById('app'));
+});
