4045eaac8fdd5388720ea2d16ac9c1e27b4331ae
CHIp Wright <frisbeeprod@msn.com>
2017-05-15 22:12:37 -0700

    initial commit

---
+++ Practice/udemy-meteor-react/3.short-link/4.styles/4.reusing-styles/imports/ui/Link.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import LinksList from './LinksList';
+import PrivateHeader from './PrivateHeader';
+import AddLink from './AddLink';
+import LinksListFilters from './LinksListFilters';
+
+export default class Link extends React.Component{
+
+  //////take page back if no auth/////
+  componentWillMount(){
+    if(!Meteor.userId()) {
+        this.props.history.replace('/');
+      }
+  }//////////////////////////////////
+
+  render(){
+    return(
+      <div>
+        <PrivateHeader title="Your links" />
+        <LinksListFilters />
+        <AddLink />
+        <LinksList key='this-key'/>
+      </div>
+    );
+  }
+}
