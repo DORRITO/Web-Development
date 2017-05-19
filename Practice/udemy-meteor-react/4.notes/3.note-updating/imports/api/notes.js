b292f8c16707d6f7717836374b14163ca5490b20
CHIp Wright <frisbeeprod@msn.com>
2017-05-19 04:32:29 -0700

    updating notes

--- Practice/udemy-meteor-react/4.notes/3.note-updating/imports/api/notes.js
+++ Practice/udemy-meteor-react/4.notes/3.note-updating/imports/api/notes.js
@@ -6,6 +6,8 @@ import SimpleSchema from 'simpl-schema';
 export const Notes = new Mongo.Collection('notes');
 
 Meteor.methods({
+
+  ///////////put in note///////////////////
   'notes.insert'() {
     if (!this.userId) {
       throw new Meteor.Error('not-authorized');
@@ -17,19 +19,56 @@ Meteor.methods({
       userId: this.userId,
       updatedAt: moment().valueOf()
     });
-  },
-  'notes.remove'(_id) {
+  },////////////////////////////////////
+
+  ////////////////////////////////////////////////
+  'notes.remove'(_id) {                         //
+    if (!this.userId) {                         //
+      throw new Meteor.Error('not-authorized'); //
+    }                                           //
+    ///////validate///////////                  //
+    new SimpleSchema({                          //
+      _id: {                                    //  remove note
+        type: String,                           //
+        min: 1                                  //
+      }                                         //
+    }).validate({_id});                         //
+                                                //
+    Notes.remove({ _id, userId: this.userId});  //
+  },/////////////////////////////////////////////
+
+  /////////////////update note////////////
+  'notes.update'({_id, updates) {
     if (!this.userId) {
       throw new Meteor.Error('not-authorized');
     }
-    ///////validate///////////
+
     new SimpleSchema({
       _id: {
         type: String,
         min: 1
+      },
+      title: {
+        type: String,
+        optional: true
+      },
+      body: {
+        type: String,
+        optional: true
       }
-    }).validate({_id});
+    }).validate({
+      _id,
+      ...updates
+    });
 
-    Notes.remove({ _id, userId: this.userId});
-  }
+    Notes.update({
+      _id,
+      userId: this.userId
+    }, {
+      $set: {
+        updatedAt: moment().valueOf(),
+        ...updates
+      }
+    });
+  }//////////////////////////////////////
 });
