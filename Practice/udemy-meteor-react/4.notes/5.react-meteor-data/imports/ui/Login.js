d9fd9e2f04fb2ca762cbc6cd614904ff8082e943
CHIp Wright <frisbeeprod@msn.com>
2017-05-19 05:22:27 -0700

    another container component for login

--- Practice/udemy-meteor-react/4.notes/5.react-meteor-data/imports/ui/Login.js
+++ Practice/udemy-meteor-react/4.notes/5.react-meteor-data/imports/ui/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Meteor} from 'meteor/meteor';
+import {createContainer} from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
 
-export default class Login extends React.Component{
+export class Login extends React.Component{
 
   //////set state/////////
   constructor(props) {
@@ -26,7 +28,7 @@ export default class Login extends React.Component{
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
-    Meteor.loginWithPassword({email}, password, (err) => {
+    this.props.loginWithPassword({email}, password, (err) => {
       err ? this.setState({error: 'Unable to login.  Check email or password'}) : this.setState({error: ''});
     });
    }/////////////////////////
@@ -52,3 +54,14 @@ export default class Login extends React.Component{
     );
   }////////////////////////////////////
 }
+
+/////////////////////////////////////////
+Login.propTypes = {
+  loginWithPassword: PropTypes.func.isRequired
+};//////////////////////////////////////////////////////////
+
+export default createContainer(() => {
+  return {
+    loginWithPassword: Meteor.loginWithPassword
+  };
+}, Login);
