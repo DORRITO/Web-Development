36b3cbdf256e57bf4f290c766455d76bfec216c8
CHIp Wright <frisbeeprod@msn.com>
2017-06-07 23:32:39 -0700

    color changes

--- Practice/meteor-react-practice/8.2quad-click-for-colors/imports/ui/Box.js
+++ Practice/meteor-react-practice/8.2quad-click-for-colors/imports/ui/Box.js
@@ -1,18 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 export default class Box extends React.Component{
-  /////////////////////////
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }/////////////////////////
-
+  
   render(){
-    var flattenStyle = require('flattenStyle'),
-    width = flattenStyle(this.props.style).width;
-    console.log(width)
     return(
-      <div ref="boxReference"></div>
+      <div></div>
     );
   }
 };
