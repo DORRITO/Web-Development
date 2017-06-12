2b2b966f414c432eabbb9ade05731dbdcff16dae
CHIp Wright <frisbeeprod@msn.com>
2017-06-11 17:56:15 -0700

    file edites

--- Practice/meteor-react-practice/8.quad-click-for-colors1/client/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import ReactDOM from 'react-dom';
-
-import { box } from '../imports/box';
-
-Meteor.startup(() => {
-    ReactDOM.render(box, document.getElementById('app'));
-});
