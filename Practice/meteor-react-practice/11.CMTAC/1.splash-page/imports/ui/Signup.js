9bb620bca7e243562f12fa9b3d0b16cc32295e1a
CHIp Wright <frisbeeprod@msn.com>
2017-06-19 04:48:32 -0700

    initial commit

---
+++ Practice/meteor-react-practice/11.CMTAC/1.splash-page/imports/ui/Signup.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router';
+
+export default class Signup extends React.Component{
+
+  /////////props///////////
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }///////////////////////
+
+  ///////////add to count////////
+  increment(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  }//////////////////////////////
+
+  ////////////////html////////////////////////////////
+  render() {
+      return(
+        <div>
+          <h1>sign up</h1>
+          <p>
+            {this.state.count}
+            <button onClick={this.increment.bind(this)}>+1</button>
+            <button onClick={() => {this.setState({count: this.state.count - 1})}}>-1</button>
+          </p>
+          <Link to="/">Back to the beginning</Link>
+        </div>
+      );
+  }//////////////////////////////////////////////
+};
