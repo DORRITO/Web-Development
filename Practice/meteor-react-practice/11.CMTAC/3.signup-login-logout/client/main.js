32ffea0489d0ff7ae5c55c65e9f8ad3b2d203fad
CHIp Wright <frisbeeprod@msn.com>
2017-06-23 05:34:23 -0700

    added unauthenticated list, and ability to check urls

--- Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/client/main.js
+++ Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/client/main.js
@@ -6,6 +6,15 @@ import {Router, Route, browserHistory} from 'react-router';
 
 import {App} from '../imports/app/App';
 
+const unauthenticatedPages = ['/', '/signup'];
+// const authenticatedPages = ['/game']
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  console.log('isAuthenticated?', isAuthenticated);
+});
+
 Meteor.startup(() => {
     Tracker.autorun(() => {
       ReactDOM.render(App, document.getElementById('app'));
