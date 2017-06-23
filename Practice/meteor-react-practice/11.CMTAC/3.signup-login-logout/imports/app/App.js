d030e87d2ceabd444702e15292d7c88af79df51b
CHIp Wright <frisbeeprod@msn.com>
2017-06-23 05:49:16 -0700

    refactored link1 to a logout ocomponent

--- Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/imports/app/App.js
+++ Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/imports/app/App.js
@@ -5,12 +5,10 @@ import {Router, Route, browserHistory} from 'react-router';
 import Default from './../ui/Default';
 import Signup from './../ui/Signup';
 import Login from './../ui/Login';
-import Link1 from './../ui/Link1';
 
 export const App = (
   <Router history={browserHistory}>
     <Route path="/" component={Default} />
-    <Route path="/link1" component={Link1} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
     <Route path="*" component={Default} />
