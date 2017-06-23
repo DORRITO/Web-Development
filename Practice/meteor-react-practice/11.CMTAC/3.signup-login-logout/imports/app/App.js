5d0361903b1ea6b4c5b89624f2c10e4c2f50308c
CHIp Wright <frisbeeprod@msn.com>
2017-06-23 06:19:21 -0700

    added main game page

--- Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/imports/app/App.js
+++ Practice/meteor-react-practice/11.CMTAC/3.signup-login-logout/imports/app/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Router, Route, browserHistory} from 'react-router';
 
-import Default from './../ui/pages/Default';
+import Splash from './../ui/pages/Default';
 import Signup from './../ui/pages/Signup';
 import Login from './../ui/pages/Login';
+import GamePage from './../ui/pages/GamePage';
 
 export const App = (
   <Router history={browserHistory}>
-    <Route path="/" component={Default} />
+    <Route path="/" component={Splash} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
-    <Route path="*" component={Default} />
+    <Route path="/gamepage" component={GamePage} />
+    <Route path="*" component={Splash} />
   </Router>
 );
