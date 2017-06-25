6326b1e5ab1fffadae49a09cb950f87039a33b46
CHIp Wright <frisbeeprod@msn.com>
2017-06-24 17:49:45 -0700

    file name change

--- Practice/meteor-react-practice/11.CMTAC/3.login-logout/imports/ui/players/Rychar.js
+++ Practice/meteor-react-practice/11.CMTAC/3.login-logout/imports/ui/players/Rychar.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {browserHistory} from 'react-router';
 import {Link} from 'react-router';
+import {Meteor} from 'meteor/meteor';
 
 export default class Rychar extends React.Component{
+
   render(){
     return(
       <div className="game-div">
+        {Meteor.userId ? <p>hello</p> : <p>no wayi</p>}
         <p>Rychar</p>
         <p><Link to="/login">Login</Link></p>
       </div>
