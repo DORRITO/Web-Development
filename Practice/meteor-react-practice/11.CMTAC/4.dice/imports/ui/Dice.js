1c6daa6c49f4cff9cc678526212607e3dd2d1f19
CHIp Wright <frisbeeprod@msn.com>
2017-07-05 19:34:32 -0700

    got number input for gm

--- Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
+++ Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
@@ -13,13 +13,16 @@ export default class Dice extends React.Component{
     return <p>dice roll is {d20()}! (+/- {this.props.modifier})</p>
   }/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  ////when the input field changes///
+  onChange(event){
+    let modifyInput = event.target.value.trim();
+    console.log(modifyInput)
+  }/////////////////////////////////
+
   /////////show modifier input the user the gm////////
   isGM(){
     if (Meteor.userId() === 'y7aACCi9zEYNc6g2p') {
-      // let modifierInput = this.refs.modInput.value.trim();
-      // console.log(modifierInput);
-
-      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={0} />
+      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={1} onChange={this.onChange}/>
       return <div>{modInput}</div>
     }
   }///////////////////////////////////////////////
