bc9848c48e30b844b064ad995db8b652c1782ca8
CHIp Wright <frisbeeprod@msn.com>
2017-07-07 20:37:04 -0700

    cleaned up modifier code

--- Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
+++ Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
@@ -7,28 +7,25 @@ export default class Dice extends React.Component{
   constructor(props){
     super(props);
       this.state = {
-        modifier: 80
+        modifier: 0
     }
   }/////////////////////
 
-  //////////////////////////////////if logged in, show dice! otherwise showlogin form///////////////////////
+  ///////////////if logged in, show dice! otherwise showlogin form//////////////////////////////////////////
   rollDice(){
     d20 = () => {
       return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
     }
     // const showLoginStatus = !!Meteor.userId() ? 'this will be a dice roll' : <Link to="/login">Login</Link>;
-    return <p>dice roll is {d20()}! (+/- {this.props.modifier})</p>
+    return <p>dice roll is {d20()}! (+/- {this.state.modifier})</p>
   }/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  ////when the input field changes///
-  onChange = (event) => {
-    let modifyInput = event ? this.setState({modifier: event.target.value}) : this.state.modifier;
-  }/////////////////////////////////
 
   /////////show modifier input the user the gm////////
   isGM(){
+    onChange = (event) => {this.setState({modifier: event.target.value})}
+
     if (Meteor.userId() === 'y7aACCi9zEYNc6g2p') {
-      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={this.state.modifier} onChange={this.onChange}/>
+      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={this.state.modifier} onChange={onChange}/>
       return <div>{modInput}</div>
     } else {
       return <div>+ {this.state.modifier}</div>
