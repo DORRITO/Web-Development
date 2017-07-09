5164a870367b218e0348cdaca7b8913e651f494c
CHIp Wright <frisbeeprod@msn.com>
2017-07-09 15:57:45 -0700

    finally finished roll functionality, and modifier hookup for it

--- Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
+++ Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
@@ -7,40 +7,39 @@ export default class Dice extends React.Component{
   constructor(props){
     super(props);
       this.state = {
-        modifier: 0
+        modifier: 0,
+        diceRoll: undefined
     }
   }/////////////////////
 
-  ///////////////if logged in, show dice! otherwise showlogin form//////////////////////////////////////////
+  ///////////////Dice, dice roll result, and modifier on dice//////////////////////////////////////////
   rollDice(){
-
+    const diceRoll = Number(this.state.diceRoll);
+    const modifier = Number(this.state.modifier);
     d20 = () => {
-      let d20 = Math.floor(Math.random() * 20 + 1)
-      console.log(d20)
-      return (
-        <div> dice roll is {d20} </div>
-      )
+      const d20 = Math.floor(Math.random() * 20 + 1 ) + modifier;
+      this.setState({diceRoll: d20})
     }
-//
+
     return(
       <div>
         <p>
-          <button onClick={d20}>Roll {d20}</button>
-          + {this.state.modifier}
+          <button onClick={d20}>Roll</button>{diceRoll ? diceRoll : ''}
+           + {this.state.modifier}
         </p>
       </div>
     )
   }/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  /////////show modifier input the user the gm////////
+  /////////show modifier input if the user is the gm////////
   isGM(){
-    onChange = (event) => {this.setState({modifier: event.target.value})}
+    changeMod = (event) => {this.setState({modifier: event.target.value})}
 
     if (Meteor.userId() === 'y7aACCi9zEYNc6g2p') {
-      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={0} onChange={onChange}/>
+      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={0} onChange={changeMod}/>
       return <div>{modInput}</div>
     }
-  }///////////////////////////////////////////////
+  }/////////////////////////////////////////////////////////
 
   render(){
     return(
