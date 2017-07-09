a73ef0b4774740b555b701567e9edcded7f46326
CHIp Wright <frisbeeprod@msn.com>
2017-07-09 11:56:28 -0700

    added modifier file, and took out modifier from dice

--- Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
+++ Practice/meteor-react-practice/11.CMTAC/4.dice/imports/ui/Dice.js
@@ -2,15 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Accounts} from 'meteor/accounts-base';
 
-export default class Dice extends React.Component{
-  ///////////////////////
-  constructor(props){
-    super(props);
-      this.state = {
-        modifier: 0
-    }
-  }/////////////////////
+import Modifer from './Modifer';
 
+export default class Dice extends React.Component{
+  
   ///////////////if logged in, show dice! otherwise showlogin form//////////////////////////////////////////
   rollDice(){
 
@@ -31,16 +26,6 @@ export default class Dice extends React.Component{
       </div>
     )
   }/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  /////////show modifier input the user the gm////////
-  isGM(){
-    onChange = (event) => {this.setState({modifier: event.target.value})}
-
-    if (Meteor.userId() === 'y7aACCi9zEYNc6g2p') {
-      const modInput = <input type="number" ref="modInput" name="modInput" placeholder={0} onChange={onChange}/>
-      return <div>{modInput}</div>
-    }
-  }///////////////////////////////////////////////
 
   render(){
     return(
