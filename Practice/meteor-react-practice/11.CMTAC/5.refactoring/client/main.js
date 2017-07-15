d29759b7d5cf090973a374085d5f5d634394d0e0
CHIp Wright <frisbeeprod@msn.com>
2017-07-15 12:34:46 -0700

    refactored from main js to app

--- Practice/meteor-react-practice/11.CMTAC/5.refactoring/client/main.js
+++ Practice/meteor-react-practice/11.CMTAC/5.refactoring/client/main.js
@@ -1,29 +1,15 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
-import {Router, Route, browserHistory} from 'react-router';
+// import {Router, Route, browserHistory} from 'react-router';
 
-import {App} from '../imports/app/App';
-
-const unauthenticatedPages = ['/', '/signup', '/login'];
-const authenticatedPages = ['/gamepage'];
+import {App, onAuthChange} from '../imports/app/App';
 
 ////////////////////update on changes///////////////////////////
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  const pathname = browserHistory.getCurrentLocation().pathname;
-
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);
-
-  if(isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/gamepage');
-  } else if(isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.replace('/');
-  }
-
-  console.log('isAuthenticated?', isAuthenticated);
+  onAuthChange(isAuthenticated);
 });///////////////////////////////////////////////////////////////////
 
 Meteor.startup(() => {
--- Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/app/App.js
+++ Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/app/App.js
@@ -7,19 +7,38 @@ import Signup from './../ui/pages/Signup';
 import Login from './../ui/pages/Login';
 import GamePage from './../ui/pages/GamePage';
 
+const unauthenticatedPages = ['/', '/signup', '/login'];
+const authenticatedPages = ['/gamepage'];
+
 /////prevent back button from going to unauthed pages////
 const onEnterPublicPage = () => {
   if(Meteor.userId()){
     browserHistory.replace('/gamepage');
   }
-}///////////////////////////////////////////////////////
+};///////////////////////////////////////////////////////
 
 /////prevent back button from going to unauthed pages////
 const onEnterPrivatePage = () => {
   if(!Meteor.userId()){
     browserHistory.replace('/');
   }
-}///////////////////////////////////////////////////////
+};///////////////////////////////////////////////////////
+
+///////////////change path to page if people are authenticated or not///////
+export const onAuthChange = (isAuthenticated) => {
+  const pathname = browserHistory.getCurrentLocation().pathname;
+
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  if(isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.replace('/gamepage');
+  } else if(isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.replace('/');
+  }
+
+  console.log('isAuthenticated?', isAuthenticated);
+};//////////////////////////////////////////////////////////
 
 export const App = (
   <Router history={browserHistory}>
