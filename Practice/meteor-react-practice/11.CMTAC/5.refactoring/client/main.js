aa4e0a240ed63da19f13c87cd4441f9ba523cc1e
CHIp Wright <frisbeeprod@msn.com>
2017-07-18 16:39:51 -0700

    moved tracker for api around

--- Practice/meteor-react-practice/11.CMTAC/5.refactoring/client/main.js
+++ Practice/meteor-react-practice/11.CMTAC/5.refactoring/client/main.js
@@ -5,18 +5,11 @@ import {Tracker} from 'meteor/tracker';
 // import {Router, Route, browserHistory} from 'react-router';
 
 import {App, onAuthChange} from '../imports/app/App';
-import {DiceData} from '../imports/api/dicedata';
 
 ////////////////////update on changes///////////////////////////
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
-});///////////////////////////////////////////////////////////////////
-
-////////////////////update on changes///////////////////////////
-Tracker.autorun(() => {
-  const dicedata = DiceData.find().fetch();
-  console.log('new dice data', dicedata);
 });///////////////////////////////////////////////////////////////////
 
 Meteor.startup(() => {
--- Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/ui/dice/ResultAndModifier.js
+++ Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/ui/dice/ResultAndModifier.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {Tracker} from 'meteor/tracker';
+
+import {DiceData} from '../../api/dicedata';
 
 export default class ResultAndModifier extends React.Component{
+  ///////////////////////
+  constructor(props){
+    super(props);
+      this.state = {
+        modifier: 0,
+        diceRoll: undefined
+    };
+  }/////////////////////
+
   componentDidMount(){
-    console.log('componentDidMount resultandmod')
+    Tracker.autorun(() => {
+      const dicedata = DiceData.find().fetch();
+      console.log('new dice data', dicedata);
+    });
   }
+
+  componentWillUnmount(){
+    console.log('component unmounted result mod component!!')
+  }
+
   render() {
     return(
       <div>
