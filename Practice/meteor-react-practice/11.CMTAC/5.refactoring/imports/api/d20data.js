f1bf90d4558fecb839cde7f7e5240b0f69729d5b
CHIp Wright <frisbeeprod@msn.com>
2017-08-15 19:32:38 -0700

    edit

--- Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/api/d20data.js
+++ Practice/meteor-react-practice/11.CMTAC/5.refactoring/imports/api/d20data.js
@@ -2,7 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 
 export const D20Data = new Mongo.Collection('d20Data');
-
+//
 if(Meteor.isServer){
   Meteor.publish('d20Data', () => {
     return D20Data.find();
