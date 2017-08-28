20067e1cd6da88cb988649b514e503c23575862d
CHIp Wright <frisbeeprod@msn.com>
2017-08-28 16:24:26 -0700

    colors to box

--- Practice/meteor-react-practice/12.center-icon/imports/ui/centerIcon.js
+++ Practice/meteor-react-practice/12.center-icon/imports/ui/centerIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class CenterIcon extends React.Component{
   render(){
     return(
-      <div className="square" className=".large-4.columns">
+      <div className="square">
         <h1 className="centerWords">hi there!</h1>
       </div>
     )
