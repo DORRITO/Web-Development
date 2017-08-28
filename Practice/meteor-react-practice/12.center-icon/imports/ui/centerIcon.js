b13ebf227542e10aa714a147db083381d601ad20
CHIp Wright <frisbeeprod@msn.com>
2017-08-27 18:08:50 -0700

    added styling

--- Practice/meteor-react-practice/12.center-icon/imports/ui/centerIcon.js
+++ Practice/meteor-react-practice/12.center-icon/imports/ui/centerIcon.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class CenterIcon extends React.Component{
   render(){
     return(
-      <div className="square">
-        <h1>hi there!</h1>
+      <div className="square" className=".large-4.columns">
+        <h1 className="centerWords">hi there!</h1>
       </div>
     )
   }
