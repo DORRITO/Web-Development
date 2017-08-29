86532a9486b33d18ced05311a94d944a897940ef
CHIp Wright <frisbeeprod@msn.com>
2017-08-28 18:57:39 -0700

    added 2nd page

--- Practice/meteor-react-practice/13.click-the-links/imports/App.js
+++ Practice/meteor-react-practice/13.click-the-links/imports/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
 
 import Hello from './ui/pages/hello';
+import OverHere from './ui/pages/overHere';
 
 export const App = (
-  <Hello />
+  <Router history={browserHistory}>
+    <Route path="/" component={Hello} />
+    <Route path="/overhere" component={OverHere} />
+    <rotue path="*" component={Hello} />
+  </Router>
 );
--- Practice/meteor-react-practice/13.click-the-links/imports/ui/pages/overHere.js
+++ Practice/meteor-react-practice/13.click-the-links/imports/ui/pages/overHere.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import {Link} from 'react-router';
-import overHere from './overHere';
 
-export default class Hello extends React.Component{
+export default class OverHere extends React.Component{
   render(){
     return(
       <div>
-        <Link to="/overhere"><h2>hiiiii</h2></Link>
         <Link to="/"><h2>over here!</h2></Link>
       </div>
     )
