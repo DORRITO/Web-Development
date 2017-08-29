a4bafd4cd0e4601bd564fed2a3d7c6f1fee267cb
CHIp Wright <frisbeeprod@msn.com>
2017-08-28 17:44:50 -0700

    removed react from center icon, added files to click the links

--- Practice/meteor-react-practice/13.click-the-links/client/main.js
+++ Practice/meteor-react-practice/13.click-the-links/client/main.js
@@ -1,1 +1,8 @@
-import './main.html';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+
+import {App} from '../imports/App';
+
+Meteor.startup(() => {
+  ReactDOM.render(App, document.getElementById('app'));
+})
---
+++ Practice/meteor-react-practice/13.click-the-links/imports/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Router, Route, browserHistory} from 'react-router';
+
+import Hello from './ui/pages/Hello';
+
+export const App = (
+  <Hello />
+);
