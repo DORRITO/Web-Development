3537d9771a8a422a98eac79cc413ec419825c659
CHIp Wright <frisbeeprod@msn.com>
2017-08-30 02:26:00 -0700

    move page on logout, but login not working

--- Practice/meteor-react-practice/14.login-signup/client/main.js
+++ Practice/meteor-react-practice/14.login-signup/client/main.js
@@ -1,7 +1,28 @@
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Router, Route, browserHistory} from 'react-router';
 
 import {App} from '../imports/ui/App';
+
+const unauthenticatedPages = ['/', 'login'];
+const authenticatedPages = ['/logout'];
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  //if its authenticated, go to logout page, if its not, go to signup page!
+  if(isUnauthenticatedPage && isAuthenticated){
+    console.log('your in');
+    browserHistory.replace('/logout');
+  } else if(isAuthenticatedPage && !isAuthenticated){
+    browserHistory.replace('/');
+  }
+});
 
 Meteor.startup(() => {
     ReactDOM.render(App, document.getElementById('app'));
--- Practice/meteor-react-practice/14.login-signup/imports/ui/App.js
+++ Practice/meteor-react-practice/14.login-signup/imports/ui/App.js
@@ -5,12 +5,14 @@ import {Router, Route, browserHistory} from 'react-router';
 
 import Signup from './signup';
 import Login from './login';
+import Logout from './logout';
 
 export const App=(
   <div>
     <Router history={browserHistory}>
       <Route path="/" component={Signup} />
       <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout} />
       <Route path="*" component={Signup} />
     </Router>
   </div>
