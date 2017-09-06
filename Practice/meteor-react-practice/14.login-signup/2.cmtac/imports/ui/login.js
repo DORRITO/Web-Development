3d2757d7558f534549c348478d83fe9a1d94fb99
CHIp Wright <frisbeeprod@msn.com>
2017-09-06 02:34:47 -0700

    added button press

--- Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/ui/login.js
+++ Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/ui/login.js
@@ -5,10 +5,21 @@ import {Link} from 'react-router';
 export default class Login extends React.Component{
 
   ////////////////////////////////
+  onSubmit(e){
+    e.preventDefault();
+  }
+  ///////////////////////////////
+
+  ////////////////////////////////
   render(){
     return(
       <div>
         <h1>Login page</h1>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input type="email" ref="email" name="email" placeholder="email" />
+            <input type="password" ref="password" name="password" placeholder="password" />
+            <button>Login</button>
+        </form>
         <Link to="/"> Back to home</Link>
         <p><Link to="/signup"> Need to make an account?</Link></p>
       </div>
--- Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/ui/signup.js
+++ Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/ui/signup.js
@@ -5,10 +5,21 @@ import {Link} from 'react-router';
 export default class Signup extends React.Component{
 
   ////////////////////////////////
+  onSubmit(e){
+    e.preventDefault();
+  }
+  ///////////////////////////////
+
+  ////////////////////////////////
   render(){
     return(
       <div>
         <h1>Signup page</h1>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input type="email" ref="email" name="email" placeholder="email" />
+            <input type="password" ref="password" name="password" placeholder="password" />
+            <button>Login</button>
+        </form>
         <p><Link to="/login">Login</Link></p>
         <Link to="/"> Need to make an account?</Link>
       </div>
