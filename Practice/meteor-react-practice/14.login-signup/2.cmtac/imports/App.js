53e28ae9af9c19f4c54d3c6da61d7fd687a64847
CHIp Wright <frisbeeprod@msn.com>
2017-09-09 18:55:12 -0700

    added logout page

--- Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/App.js
+++ Practice/meteor-react-practice/14.login-signup/2.cmtac/imports/App.js
@@ -5,6 +5,7 @@ import {Router, Route, browserHistory} from 'react-router';
 import Main from './ui/main';
 import Login from './ui/login';
 import Signup from './ui/signup';
+import Logout from './ui/logout';
 
 export const App=(
   <div>
@@ -12,6 +13,7 @@ export const App=(
       <Route path="/" component={Main} />
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout} />
       <Route path="*" component={Main} />
     </Router>
   </div>
