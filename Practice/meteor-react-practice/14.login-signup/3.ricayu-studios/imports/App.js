0259e2a4e906886435e7e7963e96a4f9f7162ffa
CHIp Wright <frisbeeprod@msn.com>
2017-09-11 00:55:04 -0700

    added navigation pages

--- Practice/meteor-react-practice/14.login-signup/3.ricayu-studios/imports/App.js
+++ Practice/meteor-react-practice/14.login-signup/3.ricayu-studios/imports/App.js
@@ -3,11 +3,17 @@ import {Meteor} from 'meteor/meteor';
 import {Router, Route, browserHistory} from 'react-router';
 
 import Main from './ui/Main';
+import About from './ui/About';
+import Gallery from './ui/Gallery';
+import MyPage from './ui/Mypage';
 
 export const App=(
   <div>
     <Router history={browserHistory}>
       <Route path="/" component={Main} />
+      <Route path="/about" component={About} />
+      <Route path="/gallery" component={Gallery} />
+      <Route path="/mypage" component={MyPage} />
       <Route path="*" component={Main} />
     </Router>
   </div>
---
+++ Practice/meteor-react-practice/14.login-signup/3.ricayu-studios/imports/ui/Mypage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class MyPage extends React.Component{
+
+  ////////////////////////////////
+  render(){
+    return(
+      <div>
+        <h1>This is the 'my page' page</h1>
+      </div>
+    );
+  }//////////////////////////////////
+}
