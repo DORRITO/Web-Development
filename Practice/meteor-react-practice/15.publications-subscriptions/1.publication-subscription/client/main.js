c119c271b69d2b4b395d466116dd1308b62bcf3e
CHIp Wright <frisbeeprod@msn.com>
2017-09-12 01:24:38 -0700

    added links data

--- Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/client/main.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/client/main.js
@@ -3,10 +3,16 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {App, onAuthChange} from '../imports/ui/App';
+import {Links} from '../imports/api/links';
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  const links = Links.find().fetch();
+  console.log('new links', links)
 });
 
 Meteor.startup(() => {
