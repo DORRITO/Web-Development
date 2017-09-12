7dbbdc7f04ea5d236d5da20cc0b36ab672f8e74c
CHIp Wright <frisbeeprod@msn.com>
2017-09-11 23:25:39 -0700

    got new app up to speed

---
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/imports/ui/Logout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Accounts} from 'meteor/accounts-base';
+
+export default class Logout extends React.Component{
+
+  onLogout(){
+    Accounts.logout();
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Your Links</h1>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
+      </div>
+    );
+  }
+}
---
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/imports/ui/Signup.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Link} from 'react-router';
+import {Accounts} from 'meteor/accounts-base';
+
+export default class Signup extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state ={
+      error: ''
+    };
+  }
+
+  ////////////////////create user///////////////////
+  onSubmit(e){
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    if(password.length < 3){
+      return this.setState({error: 'Password must be 3 chars long'});
+    }
+
+    Accounts.createUser({email, password}, (err) => {
+      if(err){
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
+  }/////////////////////////////////////////////////
+
+  ////////////////////////////////
+  render(){
+    return(
+      <div>
+        <h1>Signup page</h1>
+        {this.state.error}
+                                  {/*no validate gets rid of browser validation  */}
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input type="email" ref="email" name="email" placeholder="email" />
+            <input type="password" ref="password" name="password" placeholder="password" />
+            <button>Create account</button>
+        </form>
+        <Link to="/login"> Need to signin?</Link>
+      </div>
+    );
+  }//////////////////////////////////
+};
