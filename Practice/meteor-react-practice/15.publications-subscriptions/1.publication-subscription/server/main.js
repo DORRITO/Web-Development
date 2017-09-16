2f3930c83ff17b1027723b70c220265b71f2b412
CHIp Wright <frisbeeprod@msn.com>
2017-09-16 03:03:54 -0700

    added res/response actions

--- Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/server/main.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/server/main.js
@@ -9,11 +9,22 @@ import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-            //request comes in //run middleware one at a time  //send them that page!
+          //request comes in //run middleware one at a time  //send them that page!
   WebApp.connectHandlers.use((req, res, next) => {
     console.log('this is from my custom middleware!');
     //url: url they typed  //method: get/put/post/delete   //headers:   //query: query string (like ?=123abc)
     console.log(req.url, req.method, req.headers, req.query);
+
+    //What to do with res(ponse!)
+    //set http status code
+    // res.statusCode = 404; //for statuses! status-i?  httpstatuscodes.com
+    //set http headers
+    // res.setHeader('my-custom-header', 'CHIp was here');
+    //set http body
+    // res.write('<h1>this is the middleware taking over..at work!</h1>');  //writes to the body of the page
+    //end http request
+    // res.end();
+
     next();
   });
 });
