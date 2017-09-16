8b6a1f3bdd1e80369a5cc068cc047ba30d5ae3ff
CHIp Wright <frisbeeprod@msn.com>
2017-09-16 02:51:40 -0700

    added webapp and middleware to server

--- Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/server/main.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+//webapp allows you to attach middleware
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
 import '../imports/api/links';
@@ -7,5 +9,11 @@ import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-
+            //request comes in //run middleware one at a time  //send them that page!
+  WebApp.connectHandlers.use((req, res, next) => {
+    console.log('this is from my custom middleware!');
+    //url: url they typed  //method: get/put/post/delete   //headers:   //query: query string (like ?=123abc)
+    console.log(req.url, req.method, req.headers, req.query);
+    next();
+  });
 });
