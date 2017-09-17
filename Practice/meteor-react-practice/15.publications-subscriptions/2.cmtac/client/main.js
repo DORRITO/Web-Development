6ec507344204f1147bb66eadb3bbef0380f3b372
CHIp Wright <frisbeeprod@msn.com>
2017-09-16 22:00:32 -0700

    initial commit

---
+++ Practice/meteor-react-practice/15.publications-subscriptions/2.cmtac/client/main.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {App, onAuthChange} from './../imports/App';
+
+Meteor.startup(() => {
+    ReactDOM.render(App, document.getElementById('app'));
+});
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
