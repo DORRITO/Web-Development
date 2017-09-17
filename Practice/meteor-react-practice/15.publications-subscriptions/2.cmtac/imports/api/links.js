41eea0ef142b820ae2e6cc41cefcc2861005e75a
CHIp Wright <frisbeeprod@msn.com>
2017-09-17 00:21:09 -0700

    finished methods and publications in links file

--- Practice/meteor-react-practice/15.publications-subscriptions/2.cmtac/imports/api/links.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/2.cmtac/imports/api/links.js
@@ -10,4 +10,27 @@ if(Meteor.isServer){
     return Links.find({})
   });
 }
-//now make a method to call when the user clicks a button
+
+//now make a method to call when the user clicks a button/////
+Meteor.methods({
+  'links.insert'(url) {
+    if(!this.userId){
+      throw new Meteor.Error('not-authorized');
+    }
+    //validate what is sent
+    new SimpleSchema({
+      url: {
+        type: String,
+        label: 'Your link',
+        regEx: SimpleSchema.RegEx.Url
+      }
+    }).validate({url})
+
+    //add this it links Collection
+    Links.insert({
+      _id: shortid.generate(),
+      url,
+      userId: this.userId
+    });
+  }
+})/////////////////////////////////////////////////
