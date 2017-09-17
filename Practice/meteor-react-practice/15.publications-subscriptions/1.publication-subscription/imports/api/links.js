7fda5e41e0ae0dba637696956bc0f3e21e6bf6b9
CHIp Wright <frisbeeprod@msn.com>
2017-09-16 17:21:22 -0700

    added shortid

--- Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/imports/api/links.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.publication-subscription/imports/api/links.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
+//meteor npm install shortid
 
 //make a collection on client and server
 export const Links = new Mongo.Collection('links');
