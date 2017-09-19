c92d392c09eae4034be6daa33273c4c75bf8f887
CHIp Wright <frisbeeprod@msn.com>
2017-09-19 00:46:44 -0700

    finished picturesList

--- Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/imports/ui/components/PicturesList.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/imports/ui/components/PicturesList.js
@@ -2,4 +2,47 @@ import React from 'react';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
-import {PicturesAPI} from './../../api/links';
+import {PicturesAPI} from './../../api/picturesAPI';
+
+export default class PicturesList extends React.Component{
+
+  ////////////////////////
+    constructor(props){   //
+      super(props)        //
+        this.state = {    //
+          pictures: []       //
+        };                //
+    }///////////////////////
+
+    //////////////////////////////////////
+    componentDidMount(){
+      this.picturesTracker = Tracker.autorun(() => {
+        //subscribe/get the publication named links that you created.  which gets the api data
+        Meteor.subscribe('picturesAPI');
+        const pictures = PicturesAPI.find().fetch();
+        this.setState({ pictures });
+      });
+    }//////////////////////////////////////
+
+    //////////////////////////////////////
+    componentWillUnmount(){
+      this.linksTracker.stop();
+    }//////////////////////////////////////
+
+    //////////////////////////////////////
+    renderPicturesListItems(){
+      return this.state.pictures.map((picture) => {
+        return <p key={picture._id}>{picture.url}</p>
+      });
+    }//////////////////////////////////////
+
+    ////////////////////////////////////////////////
+    render(){
+      return(
+        <div>
+          <p>Pictures List</p>
+          <div>{this.renderPicturesListItems()}</div>
+        </div>
+      )
+    }///////////////////////////////////////////////
+}
