0fca2d8cc2aea6510d0a1a6751733c4051ea8e49
CHIp Wright <frisbeeprod@msn.com>
2017-09-19 00:52:36 -0700

    added picture list to account page

--- Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/imports/ui/Account.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/imports/ui/Account.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Accounts} from 'meteor/accounts-base';
 import {Link} from 'react-router';
 
+import PicturesAPI from './../api/picturesAPI';
+import {PicturesList} from './components/PicturesList';
+
 export default class MyPage extends React.Component{
 
   /////////////////////
@@ -9,12 +12,29 @@ export default class MyPage extends React.Component{
     Accounts.logout();
   }////////////////////
 
+  //////////////////////////////
+  onSubmit(e){
+    const picture = this.refs.picture.value.trim();
+    e.preventDefault();
+
+    if(picture){
+      Meteor.call('picturesAPI.insert', picture);
+      //Links.insert({ url, userId: Meteor.userId() });
+      this.refs.picture.value = '';
+    }
+  }///////////////////////////////
+
   ////////////////////////////////
   render(){
     return(
       <div>
         <h1>This is the account page</h1>
         <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <PicturesList />
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="picture" placeholder="Picture name" />
+          <button>Add Picture</button>
+        </form>
         <p><Link to="/login">login</Link></p>
         <p><Link to="/signup">signup</Link></p>
       </div>
