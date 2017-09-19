e98b45cff5ae1575ed789116d0966a5b228dfb57
CHIp Wright <frisbeeprod@msn.com>
2017-09-18 23:32:45 -0700

    added simple schema error reporting

--- Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/client/main.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/3.ricayu-studios/client/main.js
@@ -3,6 +3,8 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {App, onAuthChange} from './../imports/App';
+// simple schema config file turns its errors into meteor errors, so I don't have to use try catch over and over
+import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   ReactDOM.render(App, document.getElementById('app'));
