5893d41c3c615b6cb75d11841730a5115b6ae2fd
CHIp Wright <frisbeeprod@msn.com>
2017-09-23 23:49:03 -0700

    imported and added clipboard package and functionality

--- Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/LinksListItem.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/LinksListItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component{
+
+  componentDidMount(){
+    new Clipboard(this.refs.copy);
+  }
+
   render(){
     return(
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text="from meowww">Copy</button>
       </div>
     )
   }
