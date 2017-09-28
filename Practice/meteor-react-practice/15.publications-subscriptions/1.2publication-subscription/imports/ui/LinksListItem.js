0b2ef5ac2ed0f9c34fba7472f5c6df39ac282673
CHIp Wright <frisbeeprod@msn.com>
2017-09-28 15:52:02 -0700

    added live changes to copied button

--- Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/LinksListItem.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/LinksListItem.js
@@ -6,13 +6,17 @@ export default class LinksListItem extends React.Component{
 
   constructor(props){
     super(props);
+    this.state = {
+      justCopied: false
+    };
   }
 
   componentDidMount(){
     //npm install clipboard
     this.clipboard = new Clipboard(this.refs.copy);
     this.clipboard.on('success', () => {
-      alert('it worked');
+      this.setState({ justCopied: true });
+      setTimeout(() => this.setState({ justCopied: false }), 1000);
     }).on('error', () => {alert('unable to copy')})
   }
 
@@ -25,7 +29,9 @@ export default class LinksListItem extends React.Component{
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          {this.state.justCopied ? 'Copied' : 'Copy'}
+        </button>
       </div>
     )
   }
