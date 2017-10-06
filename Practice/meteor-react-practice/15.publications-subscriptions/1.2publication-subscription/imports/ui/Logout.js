df747bce6bca064943fdaca0af1aad92ba54d0b8
CHIp Wright <frisbeeprod@msn.com>
2017-10-05 17:00:17 -0700

    added linkslistfilters file to click checkbox to show lists or not

--- Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/Logout.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/1.2publication-subscription/imports/ui/Logout.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {Links} from '../api/links';
 import LinksList from './LinksList';
 import PrivateHeader from './PrivateHeader';
-import AddLink from './AddLink'
+import AddLink from './AddLink';
+import LinksListFilters from './LinksListFilters';
 
 export default () => {
     return(
       <div>
         <PrivateHeader title={"Your links from Logout!"}/>
+        <LinksListFilters />
         <AddLink />
         <LinksList />
       </div>
