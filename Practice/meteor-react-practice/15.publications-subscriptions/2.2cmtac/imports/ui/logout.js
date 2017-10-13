41e29fe62ae7f42e383361f67346de8311cf3a88
CHIp Wright <frisbeeprod@msn.com>
2017-10-13 15:35:28 -0700

    added addlink component

--- Practice/meteor-react-practice/15.publications-subscriptions/2.2cmtac/imports/ui/logout.js
+++ Practice/meteor-react-practice/15.publications-subscriptions/2.2cmtac/imports/ui/logout.js
@@ -7,27 +7,12 @@ import AddLink from './AddLink';
 
 export default class Logout extends React.Component{
 
-  //////////////////////////////
-  onSubmit(e){
-    const url = this.refs.url.value.trim();
-    e.preventDefault();
-
-    if(url){
-      Meteor.call('links.insert', url);
-      //Links.insert({ url, userId: Meteor.userId() });
-      this.refs.url.value = '';
-    }
-  }///////////////////////////////
-
   render(){
     return(
       <div>
         <PrivateHeader title="Secure logout page for the CMTAC"/>
+        <AddLink />
         <LinksList />
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL" />
-          <button>Add Link</button>
-        </form>
       </div>
     );
   }
