23906f752c88ba5cae9023061c972c89b79a40bb
CHIp Wright <frisbeeprod@msn.com>
2017-10-26 19:06:29 -0700

    boilerplate initial commit

---
+++ Practice/meteor-react-practice/Boilerplate/imports/api/links.js
@@ -0,0 +1,68 @@
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import shortid from 'shortid';//meteor npm install shortid
+
+
+//make a collection on client and server
+export const Links = new Mongo.Collection('links');
+
+if(Meteor.isServer){
+  //makes a publication, only available on server.  publications let us see api collections.
+  //'meteor remove autopublish' to remove prototyping publish (unsecure).  'meteor add accounts-password for a real one'
+  //'meteor remove insecure' to remove any clientside ability to do stuff to the data.
+          //links is the name of the publication you decide to make
+  Meteor.publish('links', function(){
+    //only returns link if they have a user id.
+    return Links.find({userId: this.userId});
+  });
+}
+
+//method naming convention:  resource.action
+//archiving emails?:         emails.archive
+Meteor.methods({
+  ///////////////insert link///////////////////////////
+  'links.insert'(url) {
+    if(!this.userId){
+      throw new Meteor.Error('not-authorized');
+    }
+    //validate what is sent!
+    new SimpleSchema({
+      url: {
+        type: String,
+        label: 'Your link', //adds your label to error/feedback
+        regEx: SimpleSchema.RegEx.Url
+      }
+    }).validate({ url })
+
+    Links.insert({
+      _id: shortid.generate(), //adds shortid id instead of meteor id
+      url,
+      userId: this.userId,
+      visible: true
+    });
+  }, ///////////////////////////////////////////////////////
+
+  ///////////////set visibility/////////////////
+  'links.setVisibility'(_id, visible){
+    if(!this.userId){
+      throw new Meteor.Error('not-authorized');
+    }
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      },
+      visible:{
+        type: Boolean
+      }
+    }).validate({ _id, visible});
+    //update, updates based off id and useris, and then sets it to visible or not
+    Links.update({
+      _id,
+      userId: this.userId
+    }, {
+      $set: {visible}
+    });
+  }///////////////////////////////////////////
+});
---
+++ Practice/meteor-react-practice/Boilerplate/imports/ui/AddLink.js
@@ -0,0 +1,74 @@
+import React from 'react';
+//meteor npm install react-modal --save
+import Modal from 'react-modal';
+import {Meteor} from 'meteor/meteor';
+
+export default class AddLink extends React.Component{
+  ////////////////////////
+  constructor(props){   //
+    super(props)        //
+      this.state = {    //
+        url: '',        //
+        isOpen: false,  //
+        error: ''       //
+      };                //
+  }///////////////////////
+
+  //////////////////////////////
+  onSubmit(e){
+    //const url = this.state.url
+    const {url} = this.state; //same thing
+    e.preventDefault();
+
+    Meteor.call('links.insert', url, (err, res) => {
+      if (!err){
+        this.handleModalClose();
+      } else {
+        this.setState({ error: err.reason });
+      }
+    });
+  }///////////////////////////////
+
+  ///////need onchange if you use a vlue field in the input, or it wont let you type////////////
+  onChange(e){
+    this.setState({
+      url: e.target.value
+    });
+  }//////////////////////////////////////////////////////////////////////////////////////////////
+
+  /////////////////////
+  handleModalClose() {
+    this.setState({
+      isOpen: false,
+      url: '',
+      error: ''
+    });
+  }///////////////////
+
+  render() {
+    return (
+      <div>
+        <button className="button" onClick={() => this.setState({isOpen: true})}>+ Add Link</button>
+        <Modal isOpen={this.state.isOpen}
+               contentLable="Add link"
+               onAfterOpen={() => this.refs.url.focus()} //puts mouse on input
+               onRequestClose={this.handleModalClose.bind(this)}
+               className="boxed_view__box"
+               overlayClassName="boxed-view boxed-view--modal">
+          <h1>Add Link</h1>
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
+          <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
+            <input type="text"
+                   ref="url"
+                   placeholder="URL"
+                   value={this.state.url}
+                   onChange={this.onChange.bind(this)} />
+            <button className="button">Add Link</button>
+            {/*putting type as button lets it ignore form submit  */}
+            <button type="button" className="button button--secondary" onClick={this.handleModalClose.bind(this)}>Close</button>
+          </form>
+        </Modal>
+      </div>
+    )
+  }
+}
