c2a59a78bdaeaa12694c0ede7b2611170530a646
CHIp Wright <frisbeeprod@msn.com>
2017-10-27 17:03:31 -0700

    changed html title and added remove notes method

--- Practice/meteor-react-practice/16.notes/1.notes/imports/api/notes.js
+++ Practice/meteor-react-practice/16.notes/1.notes/imports/api/notes.js
@@ -1,6 +1,7 @@
 import {Mongo} from 'meteor/mongo';
 import {Meteor} from 'meteor/meteor';
-import moment from 'moment';
+import SimpleSchema from 'simpl-schema';
+
 
 export const Notes = new Mongo.Collection('notes');
 
@@ -16,7 +17,22 @@ Meteor.methods({ //methods to use
       body: '',
       userId: this.userId
     });
-  }///////////////////////////////////////////
+  },///////////////////////////////////////////
 
   ///////////remove note///////////////////
+  'notes.remove'(_id){
+    if(!this.userId){
+      throw new Meteor.Error('not-authorized')
+    }
+
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({ _id });
+
+    return Notes.remove({ _id, userId: this.userId }); //this.userId makes sure it only removes that users note
+    });
+  }
 });
