269a84962b28656e0d614b0cc3bd14871b78d312
CHIp Wright <frisbeeprod@msn.com>
2017-10-28 18:25:35 -0700

    created with tracker version of container component

--- Practice/meteor-react-practice/16.notes/1.notes/imports/ui/PrivateHeader.js
+++ Practice/meteor-react-practice/16.notes/1.notes/imports/ui/PrivateHeader.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Accounts} from 'meteor/accounts-base';
+//meteor npm install react-addons-pure-render-mixin --save  (lets the next line happen)
+//meteor add react-meteor-data to use creat container
+import {createContainer} from 'meteor/react-meteor-data';
+import { withTracker } from 'meteor/react-meteor-data';
 
-const PrivateHeader = (props) => {
+export const PrivateHeader = (props) => {
   return (
     <div className="header">
       <div className="header__content">
         <h1 className="header__title">{props.title}</h1>
-        <button className="button button--link-text" onClick={() => Accounts.logout()}>Logout</button>
+        <button className="button button--link-text" onClick={() => props.handleLogout()}>Logout</button>
       </div>
     </div>
   );
 };
 
 PrivateHeader.propTypes ={
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  handleLogout: PropTypes.func.isRequired
+};
 
-export default PrivateHeader;
+export default withTracker(() => {
+  return {
+    handleLogout: () => Accounts.logout()
+  };
+})(PrivateHeader);
