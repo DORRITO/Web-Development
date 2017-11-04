d308889306bf403ee68fcb8b6d144ff4291b1e46
CHIp Wright <frisbeeprod@msn.com>
2017-11-04 02:06:45 -0700

    added body and title tracking

--- Practice/meteor-react-practice/16.notes/1.notes/imports/ui/Editor.js
+++ Practice/meteor-react-practice/16.notes/1.notes/imports/ui/Editor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Meteor} from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import {Session} from 'meteor/session';
 import PropTypes from 'prop-types';
@@ -6,10 +7,28 @@ import PropTypes from 'prop-types';
 import {Notes} from '../api/notes';
 
 export class Editor extends React.Component{
+  /////////////////////////////////////////////////////////
+  handleBodyChange(e){
+    this.props.call('notes.update', this.props.note._id, {
+      body: e.target.value
+    });
+  }//////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////
+  handleTitleChange(e){
+    this.props.call('notes.update', this.props.note._id, {
+      title: e.target.value
+    });
+  }//////////////////////////////////////////////////////////
+
   render(){
     if(this.props.note){
       return(
-        <p>We got the note!</p>
+        <div>
+          <input value={this.props.note.title} placeholder="untitles note" onChange={this.handleTitleChange.bind(this)}/>
+          <textarea value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+          <button>Delete Note</button>
+        </div>
       )
     } else {
       return(
@@ -30,6 +49,7 @@ export default withTracker(() => {
 
   return {
     selectedNoteId,
-    note: Notes.findOne(selectedNoteId)
+    note: Notes.findOne(selectedNoteId),
+    call: Meteor.call
   };
 })(Editor);
