8c3747fe780449294025e56441cbb07dbbaa2be2
CHIp Wright <frisbeeprod@msn.com>
2017-11-15 04:09:13 -0700

    got dice working

--- Practice/meteor-react-practice/11.CMTAC/6.REsplash-start/imports/ui/PlayerComponents/Player.js
+++ Practice/meteor-react-practice/11.CMTAC/6.REsplash-start/imports/ui/PlayerComponents/Player.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
+export class Player extends React.Component{
+  /////////////////////
+  constructor(props){
+    super(props);
+    this.state = {
+      d20: '',
+      modifier: 0,
+    };
+  }/////////////////////
 
-export const Player = (props) => {
+  //////////sets username////////////////////////
+  roll() {
+   this.setState({ d20: Math.floor(Math.random() * 20 + 1) })
+  }
 
-  return (
-    <div>
-      <h5>{props.name}</h5>
-      <div className="divWithPicture"></div>
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <h5>{this.props.name}</h5>
+        <div className="divWithPicture"></div>
+        <div>
+          <button onClick={this.roll.bind(this)}>Roll</button>
+          {this.state.d20}
+        </div>
+      </div>
+    );
+  }
 };
 //////////////////////////////////////////////////////////////////////
 Player.propTypes ={
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  // roll: PropTypes.func.isRequired
 };
 
 export default withTracker(() => {
