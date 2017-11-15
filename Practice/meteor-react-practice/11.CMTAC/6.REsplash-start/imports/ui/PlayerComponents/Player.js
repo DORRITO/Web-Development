bd1c8230ee4d308a07c625bbd750d6785acef0ff
CHIp Wright <frisbeeprod@msn.com>
2017-11-15 04:31:38 -0700

    input

--- Practice/meteor-react-practice/11.CMTAC/6.REsplash-start/imports/ui/PlayerComponents/Player.js
+++ Practice/meteor-react-practice/11.CMTAC/6.REsplash-start/imports/ui/PlayerComponents/Player.js
@@ -2,32 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 export class Player extends React.Component{
-  /////////////////////
+  //******************
   constructor(props){
     super(props);
     this.state = {
       d20: '',
       modifier: 0,
     };
-  }/////////////////////
+  }//*****************
 
-  //////////sets username////////////////////////
+  //**************dice roll*********************************
   roll() {
    this.setState({ d20: Math.floor(Math.random() * 20 + 1) })
-  }
+ }//*******************************************************
 
+  //************************************************************
   render(){
     return (
       <div>
         <h5>{this.props.name}</h5>
         <div className="divWithPicture"></div>
         <div>
-          <button onClick={this.roll.bind(this)}>Roll</button>
+          <button onClick={this.roll.bind(this)}>Roll +{this.state.modifier}</button>
           {this.state.d20}
+          <form><input placeholder={0} /></form>
         </div>
       </div>
     );
-  }
+  }//**********************************************************
 };
 //////////////////////////////////////////////////////////////////////
 Player.propTypes ={
