882dbf6ac027e30d7d08973c581b8212ba24deb2
CHIp Wright <frisbeeprod@msn.com>
2017-11-15 17:25:54 -0700

    added dice and gm files

---
+++ Practice/meteor-react-practice/11.CMTAC/6.REsplash-start/imports/ui/PlayerComponents/GM.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+export class GM extends React.Component{
+  //******************
+  constructor(props){
+    super(props);
+    this.state = {
+      d20: '',
+      modifier: 0,
+    };
+  }//*****************
+
+  //**************dice roll*********************************
+  roll() {
+   this.setState({ d20: Math.floor(Math.random() * 20 + 1) })
+ }//*******************************************************
+
+  //////////////////////////////////////////////////////////////////
+  render(){
+    return (
+      <div>
+        <h5>{this.props.name}</h5>
+        <div className="divWithPicture"></div>
+        <div>
+          <button onClick={this.roll.bind(this)}>Roll +{this.state.modifier}</button>
+          {this.state.d20}
+          <form><input placeholder={0} /></form>
+        </div>
+      </div>
+    );
+  }////////////////////////////////////////////////////////////////////////////
+};
+//////////////////////////////////////////////////////////////////////
+GM.propTypes ={
+  name: PropTypes.string.isRequired,
+  // roll: PropTypes.func.isRequired
+};
+
+export default withTracker(() => {
+  return {};
+})(GM);
