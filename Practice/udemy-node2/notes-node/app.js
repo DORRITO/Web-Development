8a75b5744b0e56e087620783ef5a3a53a1c34ce0
CHIp Wright <frisbeeprod@msn.com>
2017-11-27 03:47:54 -0700

    added a basic export and add function

--- Practice/udemy-node2/notes-node/app.js
+++ Practice/udemy-node2/notes-node/app.js
@@ -2,6 +2,10 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
+
+var add = notes.add(1,2);
+console.log(`Result: ${add}`);
 
 let user = os.userInfo();
 
