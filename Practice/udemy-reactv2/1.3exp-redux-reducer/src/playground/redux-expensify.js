d268dc6d5fd62b783cc8fe76150e199277161a9e
CHIp Wright <frisbeeprod@msn.com>
2017-12-04 03:05:03 -0700

    set up two reducers with combine

--- Practice/udemy-reactv2/1.3exp-redux-reducer/src/playground/redux-expensify.js
+++ Practice/udemy-reactv2/1.3exp-redux-reducer/src/playground/redux-expensify.js
@@ -1,5 +1,39 @@
 import {createStore, combineReducers} from 'redux';
 
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = [];
+
+///////////////////////build the two reducers!/////////////////////////////
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch(action.type){
+        default:
+            return{
+                text: '',
+                sortBy: 'date',
+                startDate: undefined,
+                endDate: undefined
+            }
+    }
+};
+///////////////////////////////////////////////////////////////////
+
+//combine the reducers!//////
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);///////////////////////////
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
       id: 'poijasdfhwer',
