9d88e289964f17a82fa0dfd97563d2494737c203
CHIp Wright <frisbeeprod@msn.com>
2017-12-06 01:16:57 -0700

    select menu to filter items by

--- Practice/udemy-reactv2/2.higher-order-components/src/app.js
+++ Practice/udemy-reactv2/2.higher-order-components/src/app.js
@@ -12,8 +12,8 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 5000 }));
+// store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
