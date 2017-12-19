22005ecb175f8dc4a8a79c44ed85d4a21bf7e1a3
CHIp Wright <frisbeeprod@msn.com>
2017-12-18 17:56:34 -0700

    added a private route

--- Practice/udemy-reactv2/4.0.authentication/src/routers/AppRouter.js
+++ Practice/udemy-reactv2/4.0.authentication/src/routers/AppRouter.js
@@ -6,15 +6,14 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
+import PrivateRoute from '.PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
         <Route path="/dashboard" component={ExpenseDashboardPage}/>
