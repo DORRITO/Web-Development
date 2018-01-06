d8bce2420ae19be9d3ba79ae981a086e04391f86
CHIp Wright <frisbeeprod@msn.com>
2018-01-05 22:17:55 -0700

    changed some functionality in add to functions.  Returned success or failure message to user

--- Practice/udemy-node2/1.5notes-adding-saving/notes.js
+++ Practice/udemy-node2/1.5notes-adding-saving/notes.js
@@ -1,25 +1,37 @@
 console.log('starting notes.js');
 const fs = require('fs');
 
+///////////////grab existing file//////////////////////////
+const fetchNotes = () => {
+    try{
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }catch(e){
+        return [];
+    }
+};////////////////////////////////////////////////////////
+
+///////////////write to notes-data///////////////////////
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};///////////////////////////////////////////////////////
+
+///////////////////add the note//////////////////////////////
 const addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
 
-    try{
-        let notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    }catch(e){}
-
     const duplicateNotes = notes.filter((note) => note.title === title);
 
     if(duplicateNotes.length === 0){
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
-};
+};//////////////////////////////////////////////////////////////
 
 const getAll = () => {
     console.log('Getting all notes');
