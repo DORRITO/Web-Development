72ccdc785d3afc3fa820f543575e9819735e5c58
CHIp Wright <frisbeeprod@msn.com>
2018-01-07 01:37:23 -0700

    added yargs, and encoded user input to get address

--- Practice/udemy-node2/weather/1.1encoding-user-input/app.js
+++ Practice/udemy-node2/weather/1.1encoding-user-input/app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('h', 'help')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philidelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
