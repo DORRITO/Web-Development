a73d415d1735ce88e63907a4113cc06feb69349e
CHIp Wright <frisbeeprod@msn.com>
2018-01-15 03:25:06 -0700

    added jswonwebtoken authentication, and example code

--- Practice/udemy-node2/todo/part2-auth/JWT-json-web-token/server/server.js
+++ Practice/udemy-node2/todo/part2-auth/JWT-json-web-token/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -99,8 +101,10 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user) => {
-    res.send(user);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
