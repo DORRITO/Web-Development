86ebc345703a39e0ac2e2fbc968bbcc0168cf398
CHIp Wright <frisbeeprod@msn.com>
2018-01-19 21:17:29 -0700

    have react router and node paths working together, moved app.js

--- Practice/reactv2-16-practice/create-react-&node-practice/api-hello/client/src/routers/AppRouter.js
+++ Practice/reactv2-16-practice/create-react-&node-practice/api-hello/client/src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
+import App from '../components/App';
 import Hello from '../components/Hello';
 
 export const history = createHistory();
@@ -10,7 +11,7 @@ const AppRouter = () => (
     <Router history={history}>
       <div>
         <Switch>
-          
+          <Route path="/" component={App} exact={true} />
           <Route path="/hello" component={Hello}/>
         </Switch>
       </div>
