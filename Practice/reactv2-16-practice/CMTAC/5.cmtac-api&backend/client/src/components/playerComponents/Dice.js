8475f9cfc4362f82426edf28b34f3428cbd7a930
CHIp Wright <frisbeeprod@msn.com>
2018-01-27 06:30:07 -0700

    json.sringify on body to get it working! changed api to send custom id, figuring out how to call find one with correct data

--- Practice/reactv2-16-practice/CMTAC/5.cmtac-api&backend/client/src/components/playerComponents/Dice.js
+++ Practice/reactv2-16-practice/CMTAC/5.cmtac-api&backend/client/src/components/playerComponents/Dice.js
@@ -36,31 +36,34 @@ export class Dice extends React.Component{
     return body;
   };
   callFetchAPI = async (d20) => {
-    const response = await fetch(`/players?name=${this.state.name}&dice=${d20}`, {method: 'PATCH'});
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-    // fetch('/players', { 
-    //   method: 'PATCH',
-    //   headers: {'Content-Type':'application/json'},
-    //   body: { dice: '2'} 
-    // })
+    // const response = await fetch(`/players?name=${this.state.name}&dice=${d20}`, {method: 'PATCH'});
+    
+    const response = await fetch('/players', { 
+      method: 'PATCH',
+      headers: {'Content-Type':'application/json'}, 
+      body: JSON.stringify({ name: this.state.name, dice: d20}) 
+    });
+    // const body = await response.json();
+    console.log(response.body)
+    // if (response.status !== 200) throw Error(body.message);
+    // return body;
   };
 
+  ////////dice modifier/////////
   onModifierChange(e) {
     let modifier = e.target.value
     this.setState({modifier})
-  }
+  }/////////////////////////////
 
   ///////////////////////////roll///////////////////////////////////////
   roll() {
     let d20 = Math.floor(Math.random() * 20 + 1) + Number(this.state.modifier)
     d20 < 1 ? d20 = 1 : d20 = d20
-
+    console.log('is this firing')
     this.callFetchAPI(d20)
-      .then(res => this.setState({ d20: res.Players[this.state.owner].dice }) )
-      .catch(err => console.log(err))
+    // this.setState({ d20: res.this.state.owner.dice })
+      // .then(res => console.log(res[this.state.owner]) )
+      // .catch(err => console.log(err))
   }///////////////////////////////////////////////////////////////////////
 
 //  {this.state.isGM ? <input type="number" placeholder={0} onChange={this.onModifierChange.bind(this)} value={this.state.modifier}/> : 'change this later to gm, not me'}
--- Practice/reactv2-16-practice/CMTAC/5.cmtac-api&backend/server.js
+++ Practice/reactv2-16-practice/CMTAC/5.cmtac-api&backend/server.js
@@ -6,12 +6,12 @@ var {mongoose} = require('./server/db/mongoose');
 var bodyParser = require('body-parser');
 const _ = require('lodash');
 
-
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
 
 let {PlayersAPI} = require('./server/models/players');
+PlayersAPI.findOne({_id: '123' }).then((player) => {console.log(player)})
 
 app.get('/home', (req, res) => {
   res.send({ express: 'this is the home page from the back end'});
@@ -23,26 +23,26 @@ app.get('/login', (req, res) => {
 
 app.route('/players')
   .get(function (req, res)  {
-
     PlayersAPI.find().then((players) => {
-      // console.log(players[0].Players);
       if(!players){return res.status(404).send()}
     
-      console.log(players[0].Players);
       res.send( players[0].Players );
     })
   })
   .patch(function (req, res)  {
+    // let body = _.pick(req.body, ['name', 'dice']);
+    // let userInfo = req.query; //query if using url string
+    let name = req.body.name;
+    let dice = req.body.dice;
 
-    // let userInfo = req.query;
-    // let name = userInfo.name;
-    // let dice = userInfo.dice;
-
+    PlayersAPI.findOne({name, dice}).then((players) => {
+      if(!players){return res.status(404).send()}
+      
+      res.send( players[0].Players );
+    })
     // Players[name].dice = dice;
     // console.log(Players.GM.dice, 'after!');
     
-    // let body = _.pick(req.query, ['name', 'dice']);
-    // console.log(body, 'body from pick');
     // res.send({ Players });
   })
 
