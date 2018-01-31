6250de0576d0f98739f9a95f28116a6f8d396897
CHIp Wright <frisbeeprod@msn.com>
2018-01-31 05:49:30 -0700

    got a list showing from state

--- Practice/reactv2-16-practice/CMTAC/6.cmtac-websockets/client/src/components/playerComponents/Chat.js
+++ Practice/reactv2-16-practice/CMTAC/6.cmtac-websockets/client/src/components/playerComponents/Chat.js
@@ -7,20 +7,42 @@ export class Chat extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      nextId: 0,
+      chatList: [{from:'adasd', text:'moooofasaa', createdAt: 1}, {from:'adasd222', text:'moooofasaa3333', createdAt: 2}]
     };
   }
+  
+  componentDidMount(){
+    // socket.on('newMessage', 
+    //     // function(message){
+    //     // console.log({...message})
+    //     // console.log(this.state.chatList, 'hooodalaka')
+    //     // let newArray = this.state.chatList.slice();
+    //     // newArray.push({...message});
+    //     // this.setState({chatList: this.state.chatList.push({...message}) });
+    //     // console.log({newArray}, 'hhwahhhchaa')
+    //     // this.state.chatList.push({...message});
+    // );
+  }
+//   updateChat(){console.log('ahsdjdklajksdakjsd')}
   ///////////updates text change//////////
   onTextChange(e){
     this.setState({text: e.target.value});
   }////////////////////////////////////////
 
+  ////////////////////////map throw chat state////////////////////////
+  renderChatList() {
+    return this.state.chatList.map((chatItem) => {
+        return <li key={chatItem.createdAt}>from: {chatItem.from}</li>
+    });
+  }///////////////////////////////////////////////////////////////////
+
   ///////////////////////////roll///////////////////////////////////////
-  send(e) {
+   send(e) {
     e.preventDefault();
-    console.log(e.target.value)
     socket.emit('createMessage', {
-        from: 'userreactsaywahhhh',
+        from: 'user',
         text: this.state.text
     }, function(){});
   }///////////////////////////////////////////////////////////////////////
@@ -33,6 +55,7 @@ export class Chat extends React.Component{
             <input type="text" onChange={this.onTextChange.bind(this)} placeholder="message"/>
             <button >Send chat</button>
           </form>
+          <ul>{this.renderChatList()}</ul>
         </div>
       )
   }/////////////////////////////////////////////////////////////////////////////
