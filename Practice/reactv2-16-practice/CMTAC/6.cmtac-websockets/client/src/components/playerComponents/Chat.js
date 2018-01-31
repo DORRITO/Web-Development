0bdd271a687695bc3bfe15ee17e9baa1529afe76
CHIp Wright <frisbeeprod@msn.com>
2018-01-31 02:20:27 -0700

    get text changes in react through sockets

--- Practice/reactv2-16-practice/CMTAC/6.cmtac-websockets/client/src/components/playerComponents/Chat.js
+++ Practice/reactv2-16-practice/CMTAC/6.cmtac-websockets/client/src/components/playerComponents/Chat.js
@@ -7,27 +7,22 @@ export class Chat extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      what: 'default'
+      text: ''
     };
   }
-
-  componentDidMount(){
-    // socket.on('connect', function(){
-    //   console.log('connected to server from reaeaccct');
-    // });
-  }
+  ///////////updates text change//////////
+  onTextChange(e){
+    this.setState({text: e.target.value});
+  }////////////////////////////////////////
 
   ///////////////////////////roll///////////////////////////////////////
   send(e) {
     e.preventDefault();
-
+    console.log(e.target.value)
     socket.emit('createMessage', {
         from: 'userreactsaywahhhh',
-        text: e.target.value
+        text: this.state.text
     }, function(){});
-    // let d20 = Math.floor(Math.random() * 20 + 1) + Number(this.state.modifier)
-    // d20 < 1 ? d20 = 1 : d20 = d20
-    console.log('hi')
   }///////////////////////////////////////////////////////////////////////
 
   //////////////////////////////////////////////////////////////////////////////
@@ -35,7 +30,7 @@ export class Chat extends React.Component{
       return (
         <div>
           <form onSubmit={this.send.bind(this)}>
-            <input type="text" placeholder="message"/>
+            <input type="text" onChange={this.onTextChange.bind(this)} placeholder="message"/>
             <button >Send chat</button>
           </form>
         </div>
