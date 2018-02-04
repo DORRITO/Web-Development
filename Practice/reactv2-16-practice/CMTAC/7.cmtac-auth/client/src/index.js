74d5e1b7562ce30a2a95b50230984ecb10a5c732
CHIp Wright <frisbeeprod@msn.com>
2018-02-04 00:41:19 -0700

    added redux

--- Practice/reactv2-16-practice/CMTAC/7.cmtac-auth/client/src/index.js
+++ Practice/reactv2-16-practice/CMTAC/7.cmtac-auth/client/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+    switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+    }
+  }
+
+  let store = createStore(counter)
+
+  store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch({ type: 'INCREMENT' })
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 registerServiceWorker();
