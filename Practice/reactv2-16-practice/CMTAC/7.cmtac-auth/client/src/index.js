cf0a9c16b6763655877097a978385b272d9c8209
CHIp Wright <frisbeeprod@msn.com>
2018-02-05 01:18:50 -0700

    editing store, and added router

--- Practice/reactv2-16-practice/CMTAC/7.cmtac-auth/client/src/index.js
+++ Practice/reactv2-16-practice/CMTAC/7.cmtac-auth/client/src/index.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+
+
 import { createStore } from 'redux'
 
-function counter(state = 0, action) {
-    switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-    }
-  }
+//actions
+const incrementCount = (incrementBy = {}) => ({
+    type: 'INCREMENT',
+    incrementBy : incrementBy.incrementBy
+});
 
-  let store = createStore(counter)
+const store = createStore((state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+        return {count: state.count + action.incrementBy}
+        case 'DECREMENT':
+        return state - 1
+        default:
+        return state
+    }
+});
+// let store = createStore(counter)
 
 store.subscribe(() =>
   console.log(store.getState())
 )
 
-store.dispatch({ type: 'INCREMENT' })
+store.dispatch(incrementCount({ incrementBy: 1 }))
 
+const app = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(app, document.getElementById('app'));
 registerServiceWorker();
