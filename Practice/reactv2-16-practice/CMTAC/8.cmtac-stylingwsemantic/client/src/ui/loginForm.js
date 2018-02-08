5dd8039c6c1198e03deea64095da97a28c7eb039
CHIp Wright <frisbeeprod@msn.com>
2018-02-08 01:52:23 -0700

    finished most of loginForm editing

--- Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/ui/loginForm.js
+++ Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/ui/loginForm.js
@@ -8,40 +8,17 @@ const LoginForm = () => (
       You can do same with CSS, the main idea is that all the elements up to the `Grid`
       below must have a height of 100%.
     */}
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-    <Grid
-      textAlign='center'
-      style={{ height: '100%' }}
-      verticalAlign='middle'
-    >
+    <style>{`body > div, body > div > div, body > div > div > div.login-form { height: 100%;}`}</style>
+    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-          <Image src='/logo.png' />
-          {' '}Log-in to your account
+        <Header as='h1' textAlign='center'>
+          {' '}MCMTAC Login
         </Header>
         <Form size='large'>
           <Segment stacked>
-            <Form.Input
-              fluid
-              icon='user'
-              iconPosition='left'
-              placeholder='E-mail address'
-            />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-
-            <Button color='teal' fluid size='large'>Login</Button>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username'/>
+            <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password'/>
+            <Button color='green' fluid size='large'>Enter Terraforma!</Button>
           </Segment>
         </Form>
       </Grid.Column>
