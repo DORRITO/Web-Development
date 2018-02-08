e23392ea1c21df6c10fc77e20a4ebd309e47a668
CHIp Wright <frisbeeprod@msn.com>
2018-02-08 04:58:53 -0700

    header setup for gamepage

--- Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/components/GamePage.js
+++ Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/components/GamePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {history} from './../routers/AppRouter';
 // import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
 
 import Player from './playerComponents/Player';
   class GamePage extends Component {
@@ -17,10 +18,12 @@ import Player from './playerComponents/Player';
     render() {
       return (
         <div>
-          <h2>Terraria</h2>
-          <button onClick={this.onClick.bind(this)}>Logout</button>
-          <h1>Presented by the Mayors Cross Map Townsmanship Aficionados Club</h1>
-          <h5>(In cooperation with the arcane order)</h5>
+          <Menu widths="9" style={{backgroundColor: '#65e69f', color: '#4b220a', height: '4rem'}} >
+            <h2>Terraforma</h2>
+            <div><h1 className="Header-gamepage">Presented by the Mayors Cross Map Townsmanship Aficionados Club</h1>
+            <h5>(In cooperation with the arcane order)</h5></div>
+            <Menu.Item header onClick={this.onClick.bind(this)}>Logout</Menu.Item>
+          </Menu>
           <div className="playersSection">
             <Player name="Rychar" icon="I'm a sign and building" />
             <Player name="Tylendel" icon="I'm a castle" />
