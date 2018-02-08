4d43d6937626d0461980e08564496598278374ff
CHIp Wright <frisbeeprod@msn.com>
2018-02-08 02:17:27 -0700

    moved functionality to login form, commented it out in login

--- Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/ui/loginForm.js
+++ Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/ui/loginForm.js
@@ -1,29 +1,79 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import titleCase from 'title-case';
+import getName from './../redux/actions/nameAction';
+import {history} from './../routers/AppRouter';
 
-const LoginForm = () => (
-  <div className='login-form'>
-    {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-    <style>{`body > div, body > div > div, body > div > div > div.login-form { height: 100%;}`}</style>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h1' textAlign='center'>
-          {' '}MCMTAC Login
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username'/>
-            <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' type='password'/>
-            <Button color='green' fluid size='large'>Enter Terraforma!</Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-)
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
 
+const loginFail = 'This login or password is not registered with the MCMTAC. \n\nIf you enter at this point, a member of the arcane order may incinerate you. While some of our citizens might try a taste of a well done version of you, We like you the way you are, please register!'
+
+class LoginForm extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        login: '',
+        password: '',
+    };
+  }
+  
+  /////////////////////////////////////////////////////
+  callApi = async () => {
+    const response = await fetch('/login');
+    const body = await response.json();
+    
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };//////////////////////////////////////////////////////
+
+  ///////////updates text change//////////
+  onLoginChange(e){
+    let login = e.target.value.trim().toLowerCase()
+    this.setState({login});
+  }////////////////////////////////////////
+
+  ///////////updates text change//////////
+  onPassChange(e){
+    let password = e.target.value.trim().toLowerCase()
+    this.setState({password});
+  }////////////////////////////////////////
+
+  ///////////updates text change//////////
+  onSubmit(e){
+    e.preventDefault();
+    let capLogin = titleCase(this.state.login);
+    let lowerPass = this.state.password.toLowerCase();
+    
+    this.callApi()
+      .then(res => { 
+        if(res[capLogin].password === lowerPass){
+          this.props.dispatch(getName({ name: capLogin, auth: true }));
+          history.push('/gamepage')
+        }else{alert(loginFail)}
+      })
+      .catch(err => alert(loginFail));
+  }////////////////////////////////////////
+
+  render() {
+    return (
+      <div className='login-form'>
+        <style>{`body > div, body > div > div, body > div > div > div.login-form { height: 100%;}`}</style>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h1' textAlign='center'>
+            {' '}MCMTAC Login
+            </Header>
+            <Form size='large' onSubmit={this.onSubmit.bind(this)} noValidate>
+            <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' onChange={this.onLoginChange.bind(this)} placeholder='Username'/>
+                <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' onChange={this.onPassChange.bind(this)} type='password'/>
+                <Button color='green' fluid size='large'>Enter Terraforma!</Button>
+            </Segment>
+            </Form>
+        </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 export default LoginForm;
