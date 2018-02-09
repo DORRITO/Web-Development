84fda3e3174f3888ed6f5ecad980263d100f808c
CHIp Wright <frisbeeprod@msn.com>
2018-02-09 03:00:21 -0700

    setting up file for incapacitate for future component

--- Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/components/playerComponents/Player.js
+++ Practice/reactv2-16-practice/CMTAC/8.cmtac-stylingwsemantic/client/src/components/playerComponents/Player.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import openSocket from 'socket.io-client';
 import {connect} from 'react-redux';
 
+import incapacitate from '.Incapacitate';
 import Dice from './Dice';
 import Chat from './Chat';
 import PlayerBoxIcon from './PlayerBoxIcon';
@@ -16,21 +17,22 @@ class Player extends React.Component{
     this.state = {
       d20: '',
       isChecked: false,
-      modifier: 0
+      modifier: 0,
+      knockedOut: ''
     };
   }//*****************
 
   componentDidMount(){
-    socket.on('incapacitated2', (data) => {if(data.name === this.props.user){this.setState({isChecked: data.isChecked})} });
+    socket.on('incapacitated2', (data) => {if(data.name === this.props.user){this.setState({isChecked: data.isChecked, knockedOut: data.name})} });
     // socket.on('incapacitated', (data) => {if(data.name === this.state.owner){this.setState({d20: data.dice})} });
   }//////////////////////////////////////////////////////////////////////////
 
-    //**********************************
-    onCheckboxChange = (e) => {
-        let isChecked = e.target.checked;
-        let name = this.props.name;
-        socket.emit('incapacitated', isChecked, name);
-    }//**********************************
+  //**********************************
+  onCheckboxChange = (e) => {
+      let isChecked = e.target.checked;
+      let name = this.props.name;
+      socket.emit('incapacitated', isChecked, name);
+  }//**********************************
 
   //////////////////////////////////////////////////////////////////
   render(){
@@ -41,7 +43,7 @@ class Player extends React.Component{
         <PlayerBoxIcon icon={this.props.icon}/>
         </div>
         <div className="divWithbackground">
-            {!this.state.isChecked ? <Dice owner={this.props.name}/> : 'INCAPACITATED'}
+            {this.state.isChecked && this.props.user === this.state.knockedOut ? 'INCAPACITATED' : <Dice owner={this.props.name}/>}
         </div> 
         {this.props.user === 'Gm' ? <div><input type="checkbox" onChange={this.onCheckboxChange.bind(this)}></input>FINISH HIM</div> : ''}
         {this.props.name === this.props.user || this.props.user === 'Gm' ? <Chat owner={this.props.name}/> : ''}
