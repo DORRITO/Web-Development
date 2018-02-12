ed895f6036775348ddd7559433f2d62a3b1f99aa
CHIp Wright <frisbeeprod@msn.com>
2018-02-12 01:46:23 -0700

    added temp image, set box in middle of the screen and made it transparent

--- Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
+++ Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import GridRow, { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
-const circle = { width: 200, height: 200, backgroundColor: "blue" }
+const circle = { width: 200, height: 200 }
 
 const AppHeader = () => (
-  <Header >Yesss
-    <Segment circular style={circle}></Segment>
+  <Header>
+    <Grid centered columns={2}>
+        <Segment className="Centered-Box" style={{marginTop: '10rem', background: '#D6E3E8BF'}} padded>
+          <Grid.Row centered >
+            <Grid.Column textAlign='center'>
+                <h2>Hello, I'm Lance!</h2> 
+                <h3>A Jr. Developer from the great state of Arizona who is looking for my first web developer job after making a career switch.</h3>
+          </Grid.Column>
+        </Grid.Row>
+        </Segment>
+        <Grid.Row centered>
+            <Grid.Column textAlign='center'>
+                Here is my profile
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
   </Header>
 )
 
 export default AppHeader;
+
+// <Segment circular style={circle}></Segment>
