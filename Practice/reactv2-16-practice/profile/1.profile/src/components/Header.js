2d815e31284749393c91b9ecdfda3308c9b5918a
CHIp Wright <frisbeeprod@msn.com>
2018-02-11 23:20:02 -0700

    added background, set it up and is currently opaque

---
+++ Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
+const AppHeader = () => (
+  <Header inverted color={'green'}>Yesss</Header>
+)
+
+export default AppHeader;
