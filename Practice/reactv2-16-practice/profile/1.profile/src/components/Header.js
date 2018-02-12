2fcbdeb962f06a52e3d9a8e5da784d65341cfe1b
CHIp Wright <frisbeeprod@msn.com>
2018-02-12 02:38:26 -0700

    left box

--- Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
+++ Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
@@ -6,7 +6,7 @@ const circle = { width: 200, height: 200 }
 const AppHeader = () => (
   <Header>
     <Grid centered columns={2}>
-        <Segment className="Centered-Box" style={{marginTop: '7%', background: '#D6E3E8B0'}} padded>
+        <Segment className="Centered-Box" style={{marginTop: '7%', background: '#D6E3E8B3'}} padded>
           <Grid.Row centered >
             <Grid.Column textAlign='center'>
                 <h2>Hello, I'm Lance!</h2> 
@@ -15,10 +15,27 @@ const AppHeader = () => (
           </Grid.Column>
         </Grid.Row>
         </Segment>
-        <Grid.Row centered columns={6}>
-            <Grid.Column textAlign='center'>
-                Here is my profile
-            </Grid.Column>
+        <Grid.Row centered columns={2} className='Display' style={{ marginTop: '3%'}}>
+            <Segment className='Display' style={{ background: '#D6E3E8B3', paddingRight: '3rem'}} stacked>
+                <Grid.Column textAlign='left'>
+                <h3>Areas of knowledge</h3>
+                <ul>
+                    <li>HTML &emsp; Node.JS &emsp; Github</li>
+                    <li>&emsp;&nbsp;CSS &emsp; MongoDB &emsp; Meteor.JS</li>
+                    <li>&emsp;&nbsp;Semantic UI &emsp; React &emsp; Redux</li>
+                    <li>&emsp;&emsp;&emsp;&emsp;JavaScript &emsp; Socket.IO &emsp; Mongoose</li>
+                </ul>
+                </Grid.Column>
+            </Segment>
+            <Segment className='Display' style={{ background: '#D6E3E8B3', marginLeft: '15%'}} stacked>
+                <Grid.Column textAlign='left'>
+                <h3>Websites:</h3>
+                <div>
+                <h5 className="Display">MCMTAC:</h5>
+                hi
+                </div>
+                </Grid.Column>
+            </Segment>
         </Grid.Row>
     </Grid>
   </Header>
