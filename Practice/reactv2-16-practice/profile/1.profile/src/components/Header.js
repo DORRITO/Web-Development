6391763326b4901ecd9d1062f074bfa0fa1406ea
CHIp Wright <frisbeeprod@msn.com>
2018-02-12 03:56:55 -0700

    page finished for now

--- Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
+++ Practice/reactv2-16-practice/profile/1.profile/src/components/Header.js
@@ -8,19 +8,20 @@ const circle = { width: 200, height: 200 }
 const AppHeader = () => (
   <Header>
     <Grid centered columns={2}>
-        <Segment className="Centered-Box" style={{marginTop: '5%', background: '#D6E3E8B3'}} padded>
+        <Segment style={{marginTop: '2%', background: '#D6E3E8B3'}} padded>
           <Grid.Row centered >
             <Grid.Column textAlign='center'>
                 <h2>Hello, I'm Lance!</h2> 
-                <Image src={profileImage} size='small' centered circular />
-                <h3>A Jr. Developer from the great state of Arizona who is looking for my first web developer job after making a career switch.</h3>
-                <h5 style={{marginTop: '-1%'}} >I enjoy Sports, playing music, writing, chasing around my cats, and of course, coding!</h5>
+                <Image style={{marginTop: '-1.5%'}} src={profileImage} size='small' centered circular />
+                <h3 style={{marginTop: '0%'}}>A Jr. Developer from the great state of Arizona who is looking for my first web developer job after making a career switch.</h3>
+                <h5 style={{marginTop: '-1%', marginBottom: '-2%'}} >I enjoy Sports, playing music, writing, playing games, chasing around my cats, and of course, coding!</h5>
           </Grid.Column>
         </Grid.Row>
         </Segment>
-        <Grid.Row centered columns={2} className='Display' style={{ marginTop: '3%'}}>
-            <Segment className='Display' style={{ background: '#D6E3E8B3', paddingRight: '3rem'}} stacked>
-                <Grid.Column textAlign='left'>
+        <Grid.Row centered columns={2} className='Display'>
+            <Segment className='Display' style={{ background: '#D6E3E8B3'}} stacked>
+                <Grid.Column style={{marginBottom: '-4%'}}>
+                <Segment color='orange'>
                 <h3>Areas of knowledge</h3>
                 <ul>
                     <li>HTML &emsp; Node.JS &emsp; Github</li>
@@ -28,10 +29,11 @@ const AppHeader = () => (
                     <li>&emsp;&nbsp;Semantic UI &emsp; React &emsp; Redux</li>
                     <li>&emsp;&emsp;&emsp;&emsp;JavaScript &emsp; Socket.IO &emsp; Mongoose</li>
                 </ul>
+                </Segment>
                 </Grid.Column>
             </Segment>
             <Segment className='Display' style={{ background: '#D6E3E8B3', marginLeft: '10%'}} stacked>
-                <Grid.Column textAlign='left'>
+                <Grid.Column>
                 <h3>Websites:</h3>
                 <Segment color='orange' >
                     <Button color='teal' href="https://mcmtac.herokuapp.com/" target="_blank">MCMTAC</Button>
