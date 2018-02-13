56035c16d4b98ebbfb1d8e6e91d467ea8a52f7c5
CHIp Wright <frisbeeprod@msn.com>
2018-02-13 01:13:44 -0700

    dark static header with orange coloring added

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/Header.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/Header.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Button, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Button, Container, Dropdown, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react'
 
 // import profileImage from './../images/profileimage.jpg';
 
 const AppHeader = () => (
-  <Header>
-    hi
-  </Header>
+    <Menu fixed='top' inverted>
+    <Container>
+      <Menu.Item as='a' header>
+        <Image size='mini' src='/logo.png' style={{ marginRight: '1.5rem', marginLeft: '-9rem' }} />
+        <div className="Main-Theme">Ricayu Studios</div>
+      </Menu.Item>
+      <Menu.Item as='a' style={{ color: '#E0661F' }}>Home</Menu.Item>
+      <Dropdown item simple text='Dropdown' style={{ color: '#E0661F' }}>
+        <Dropdown.Menu>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Header Item</Dropdown.Header>
+          <Dropdown.Item>
+            <i className='dropdown icon' />
+            <span className='text'>Submenu</span>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
+  </Menu>
 )
 
 export default AppHeader;
