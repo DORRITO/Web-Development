38b250288ef97883f6740b72e463743a36ffd872
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 04:15:04 -0700

    added portraits table props

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Portraits.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Portraits.js
@@ -4,12 +4,15 @@ import React from 'react'
 // import profileImage from './../images/profileimage.jpg';
 import Services from './Services';
 
+const servNamesArr = ['Studio Photo Shoot', 'Select Printed Proofs', 'On Location Shoot', 'Second Location', 'Digital Proofs of all Photos', 'Hardbound Photo Book', 'Outfit Changes (Silver: 1, Gold: 2, Plat: 3)'];
+const silvArr = [true, true, false, false, false, false, true, '$175'];
+const goldArr = [true, true, true, false, false, false, true, '$275'];
+const platArr = [true, true, true, true, true, true, true, '$500'];
+
 const Portraits = () => (
     <div>
-    <h1>CHIGGA BOOM contact</h1>
-    <h1>CHIGGA BOOM contact</h1>
-    <h1>CHIGGA BOOM portrait</h1>
-    <Services />
+        <h1>CHIGGA BOOM contact</h1>
+        <Services serviceNames={servNamesArr} silver={silvArr} gold={goldArr} plat={platArr}/>
     </div>
 )
 
