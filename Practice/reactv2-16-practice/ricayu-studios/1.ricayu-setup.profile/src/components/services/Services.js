e6097dd52307d0deac8edefeae8417d996361b0b
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 02:45:58 -0700

    full list of wedding services (left hand side) added

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Services.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Services.js
@@ -14,19 +14,49 @@ const Services = (props) => (
           </Table.Row>
         </Table.Header>
 
-        <Table.Body sortable>
+        <Table.Body>
           <Table.Row>
-            <Table.Cell>Apples</Table.Cell>
+            <Table.Cell>{props.serviceNames[0]}</Table.Cell>
             <Table.Cell>200</Table.Cell>
             <Table.Cell>0g</Table.Cell>
           </Table.Row>
           <Table.Row>
-            <Table.Cell>Orange</Table.Cell>
+            <Table.Cell>{props.serviceNames[1]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{props.serviceNames[2]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{props.serviceNames[3]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{props.serviceNames[4]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{props.serviceNames[5]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{props.serviceNames[6]}</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell></Table.Cell>
             <Table.Cell>310</Table.Cell>
             <Table.Cell>0g</Table.Cell>
           </Table.Row>
         </Table.Body>
-      </Table>
+    </Table>
 )
 
 export default Services;
