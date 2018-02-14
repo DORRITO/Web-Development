407da9945142afbffa76811a8c7276c19baa8837
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 03:09:00 -0700

    wedding silver and gold arrays made and passed as props

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Weddings.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Weddings.js
@@ -5,12 +5,14 @@ import React from 'react'
 import Services from './Services';
 
 const servNamesArr = ['Ceremony Photos', 'Group Photos Pictures', 'Select Printed Proofs', 'Reception Pictures', 'Hardbound Photo Book', 'Digital Proofs of all Photos', 'Second Photographer'];
-const silvArr =[true, true, true, false, false, false, false, '$1200']
+const silvArr = [true, true, true, false, false, false, false, '$1200'];
+const goldArr = [true, true, true, true, true, false, false, '$2000'];
+const platArr = [true, true, true, true, true, true, true, '$2500'];
 
 const Weddings = () => (
     <div>
     <h1>CHIGGA BOOM weddings</h1>
-        <Services serviceNames={servNamesArr} silver={silvArr}/>
+        <Services serviceNames={servNamesArr} silver={silvArr} gold={goldArr} plat={goldArr}/>
     </div>
 )
 
