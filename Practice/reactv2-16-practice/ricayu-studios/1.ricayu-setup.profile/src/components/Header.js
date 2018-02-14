780778643c73a35286bae506241883f120d6b27c
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 01:33:26 -0700

    added facebook, youtube etc.. links. moved contact me to right side

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/Header.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/Header.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-import {Container, Dropdown, Header, Image, Menu} from 'semantic-ui-react'
+import {Container, Dropdown, Header, Image, Icon, Menu} from 'semantic-ui-react'
 
 // import profileImage from './../images/profileimage.jpg';
 
 const AppHeader = () => (
     <Header>
     <Menu fixed='top' inverted>
-    <Container>
-      <Link to="/"><Menu.Item as='a' style={{  marginLeft: '-6rem', color: '#FFA60D'}}>Ricayu Studios</Menu.Item></Link>
+      <Link to="/"><Menu.Item as='a' style={{ color: '#FFA60D'}}>Ricayu Studios</Menu.Item></Link>
       <Link to="/"><Menu.Item as='a' style={{ color: '#FFA60D' }}>Home</Menu.Item></Link>
       <Link to="/about"><Menu.Item as='a' style={{ color: '#FFA60D' }}>About Me</Menu.Item></Link>
-      <Dropdown item simple text='Services' style={{ color: '#FFA60D' }}>
+      <Dropdown item simple text='Services' style={{ color: '#FFA60D', marginTop: '-.5rem' }}>
         <Dropdown.Menu>
             <Link to="/weddings"><Dropdown.Item style={{ color: 'black' }}>Weddings</Dropdown.Item></Link>
           <Dropdown.Divider />
@@ -20,8 +19,16 @@ const AppHeader = () => (
             <Link to="/events"><Dropdown.Item style={{ color: 'black' }}>Events/Sports Pictures</Dropdown.Item></Link>
         </Dropdown.Menu>
       </Dropdown>
-      <Link to="/contact"><Menu.Item as='a' style={{ color: '#FFA60D' }}>Contact Me</Menu.Item></Link>
-    </Container>
+
+    <Menu.Menu position='right'>
+        <Link to="/contact"><Menu.Item as='a' style={{ color: '#FFA60D' }}>Contact Me</Menu.Item></Link>
+        <Menu.Item as='a' >
+            <Icon name='facebook' size='large' color='blue'/>
+            <Icon name='twitter' size='large' inverted color='blue'/>
+            <Icon name='youtube' size='large' inverted color='red'/>
+            <Icon name='instagram' size='large' inverted color='pink'/>
+        </Menu.Item>
+    </Menu.Menu>
   </Menu>
   </Header>
 )
