39f4255f5aaa4eb7b5b892478e7602f801fcb067
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 02:39:48 -0700

    added colored table to services, and it"s titles.

--- Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Services.js
+++ Practice/reactv2-16-practice/ricayu-studios/1.ricayu-setup.profile/src/components/services/Services.js
@@ -1,15 +1,32 @@
-import React from 'react'
-// import {Container, Dropdown, Header, Image, Menu} from 'semantic-ui-react'
+import React from 'react';
+import {Table, Icon} from 'semantic-ui-react';
 
 // import profileImage from './../images/profileimage.jpg';
-
+// {props.serviceNames[0]}
 const Services = (props) => (
-    <div>
-    <h1>CHIGGA BOOM contact</h1>
-    <h1>CHIGGA BOOM contact</h1>
-    <h1>CHIGGA BOOM servicceeee</h1>
-    <h3>{props.serviceNames[0]}</h3>
-    </div>
+    <Table color='orange' sortable>
+        <Table.Header >
+          <Table.Row >
+            <Table.HeaderCell>Services</Table.HeaderCell>
+            <Table.HeaderCell>Silver</Table.HeaderCell>
+            <Table.HeaderCell>Gold</Table.HeaderCell>
+            <Table.HeaderCell>Platinum</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body sortable>
+          <Table.Row>
+            <Table.Cell>Apples</Table.Cell>
+            <Table.Cell>200</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Orange</Table.Cell>
+            <Table.Cell>310</Table.Cell>
+            <Table.Cell>0g</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
 )
 
 export default Services;
