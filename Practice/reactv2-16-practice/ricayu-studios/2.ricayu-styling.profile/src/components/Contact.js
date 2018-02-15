48908d7575f56c4f1ab3a71005e786c584d95534
CHIp Wright <frisbeeprod@msn.com>
2018-02-15 02:25:17 -0700

    added background to contact

--- Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Contact.js
+++ Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Contact.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Contact = () => (
-    <div>
-        <h1>CHIGGA BOOM contact</h1>
-        <h1>CHIGGA BOOM contact</h1>
-        Ricayu Studios
-        Yuma AZ 85364
-        Phone: 928-580-3048 
-        E-mail: ricayustudios@gmail.com
+    <div className='BackgroundBlack' >
+        <div className="BackgroundImage">
+        <h1></h1><h1></h1>
+            <div className="Theme-White">
+                <h1>CHIGGA BOOM contact</h1>
+                <h1>CHIGGA BOOM contact</h1>
+                Ricayu Studios
+                Yuma AZ 85364
+                Phone: 928-580-3048 
+                E-mail: ricayustudios@gmail.com
+            </div>
+        </div>
     </div>
 )
 
