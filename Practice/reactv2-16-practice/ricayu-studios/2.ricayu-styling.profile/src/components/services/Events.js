2aeb33e0d89dd18d869934a560754bc9be9e50ed
CHIp Wright <frisbeeprod@msn.com>
2018-02-15 02:12:48 -0700

    setup background for about me and events

--- Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/services/Events.js
+++ Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/services/Events.js
@@ -5,10 +5,12 @@ import {Header, Image} from 'semantic-ui-react'
 import Services from './Services';
 
 const Events = () => (
-    <div>
-        <h1>CHIGGA BOOM events</h1>
-        <Header as='h1' textAlign='center'>Events/Sports Pictures:</Header>
-        <Header as='h5' textAlign='center'>Usually shot hourly: contact me for specific rates!</Header>
+    <div className='BackgroundBlack' >
+        <div className="BackgroundImage">
+            <h1>CHIGGA BOOM events</h1>
+            <Header style={{color: 'white'}} as='h1' textAlign='center'>Events/Sports Pictures:</Header>
+            <Header style={{color: 'white'}} as='h5' textAlign='center'>Usually shot hourly: contact me for specific rates!</Header>
+        </div>
     </div>
 )
 
