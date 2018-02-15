866028183c1229cf7d47661e28c52b73057d69bb
CHIp Wright <frisbeeprod@msn.com>
2018-02-15 05:58:33 -0700

    event page finished

--- Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/services/Events.js
+++ Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/services/Events.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import {Header, Image} from 'semantic-ui-react'
+import {Card, Header} from 'semantic-ui-react'
 
-// import profileImage from './../images/profileimage.jpg';
 import Services from './Services';
 
 const Events = () => (
     <div className='BackgroundBlack' >
         <div className="BackgroundImage">
-            <h1>CHIGGA BOOM events</h1>
-            <Header style={{color: 'white'}} as='h1' textAlign='center'>Events/Sports Pictures:</Header>
-            <Header style={{color: 'white'}} as='h5' textAlign='center'>Usually shot hourly: contact me for specific rates!</Header>
+        <h1></h1><h1></h1>
+            <div className='Theme-Events'>
+                <Card fluid centered raised color='orange'>
+                    <Card.Content textAlign='center'>
+                        <Card.Header>Events/Sports Pictures:</Card.Header>
+                        <Card.Description as='h4'>Usually shot hourly: contact me for specific rates!</Card.Description>
+                    </Card.Content>
+                </Card>
+            </div>
         </div>
     </div>
 )
 
 export default Events;
+
+// <Header style={{color: 'white'}} as='h1' textAlign='center'>Events/Sports Pictures:</Header>
+//             <Header style={{color: 'white'}} as='h5' textAlign='center'>Usually shot hourly: contact me for specific rates!</Header>
