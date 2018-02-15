69c991c9dba31869c52b5b71cbbd827e7e8c57ea
CHIp Wright <frisbeeprod@msn.com>
2018-02-15 06:04:30 -0700

    contact page style finished

--- Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Contact.js
+++ Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Contact.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import {Card} from 'semantic-ui-react';
 
 const Contact = () => (
     <div className='BackgroundBlack' >
         <div className="BackgroundImage">
         <h1></h1><h1></h1>
-            <div className="Theme-White">
-                <h1>CHIGGA BOOM contact</h1>
-                <h1>CHIGGA BOOM contact</h1>
-                Ricayu Studios
-                Yuma AZ 85364
-                Phone: 928-580-3048 
-                E-mail: ricayustudios@gmail.com
+            <div className='Theme-Events'>
+                <Card fluid centered raised color='orange'>
+                    <Card.Content textAlign='center'>
+                        <Card.Header>Ricayu Studios</Card.Header>
+                        <Card.Header>Yuma AZ 85364</Card.Header>
+                        <Card.Header>Phone: 928-580-3048</Card.Header>
+                        <Card.Header>E-mail: ricayustudios@gmail.com</Card.Header>
+                    </Card.Content>
+                </Card>
             </div>
         </div>
     </div>
