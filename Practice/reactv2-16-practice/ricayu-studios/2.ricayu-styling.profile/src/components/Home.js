8c2431ccad876833fbb7b0d8a18140d21e66cd9a
CHIp Wright <frisbeeprod@msn.com>
2018-02-14 20:20:04 -0700

    temporary carousel and set up carousel styles

--- Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Home.js
+++ Practice/reactv2-16-practice/ricayu-studios/2.ricayu-styling.profile/src/components/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Fade} from 'react-slideshow-image';
 
-// import { Header } from 'semantic-ui-react'
-// className="BackgroundImage"
+import band from './../images/band.jpg';
+import cake from './../images/cake.jpg';
+import engagement from './../images/engagement.jpg';
+import football from './../images/football.jpg';
+import oceanportrait from './../images/oceanportrait.jpg';
+import tie from './../images/tie.jpg';
+
+const images = [band, engagement,football, oceanportrait];
+
 const Home = () => (
     <div className='BackgroundBlack'>
         <div className="BackgroundImage">
             <h1></h1><h1></h1>
             <h1 className="Theme-White" className='Theme-Title'>Capturing the Magic of Every Day.</h1>
+            <div className="Theme-Carousel"><Fade images={images} duration="5000" transitionDuration="1000"/></div>
         </div>
     </div>
 )
