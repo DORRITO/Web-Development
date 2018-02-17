588be88a6c2d1fcf49368055fe828d96c4ae9efa
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 03:54:40 -0700

    added full needed semantic ui. set up grid

--- Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
+++ Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hi
+        <Grid container columns={2} centered>
+          <Grid.Column className="Centered" inverted="true" >
+            hi
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
