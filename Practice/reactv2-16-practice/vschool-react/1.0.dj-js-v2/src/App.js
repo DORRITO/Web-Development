bebe8156b56184f3bbdbd50821625c39d8bc4347
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 06:28:59 -0700

    3 hours later, got sematic ui in center of screen

--- Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
+++ Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Segment } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Grid container columns={2} centered>
-          <Grid.Column className="Centered" inverted="true" >
-            hi
-          </Grid.Column>
-        </Grid>
+        <div className="Centered">
+          <Grid className="Boxes" basic container columns={3} padded>
+            <Segment.Group horizontal className='Inside'>
+                <Segment color='blue' inverted></Segment>
+                <Segment color='green' inverted></Segment>
+                <Segment color='red' inverted></Segment>
+            </Segment.Group>
+          </Grid>
+        </div>
+
+        <div className="Centered2">
+          <Grid className="Boxes" basic container columns={3} padded>
+            <Segment.Group horizontal className='Inside'>
+                <Segment color='orange' inverted></Segment>
+                <Segment color='yellow' inverted></Segment>
+                <Segment color='olive' inverted></Segment>
+            </Segment.Group>
+          </Grid>
+        </div>
       </div>
     );
   }
