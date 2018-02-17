58f9a1619e011739e881a631ce6b73b59c2426de
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 08:48:46 -0700

    finally changing on hover, had to switch to div instead of semantic react

--- Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
+++ Practice/reactv2-16-practice/vschool-react/1.0.dj-js-v2/src/App.js
@@ -7,21 +7,21 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Centered">
-          <Grid className="Boxes" basic container columns={3} padded>
+          <Grid className="Boxes" container columns={3} >
             <Segment.Group horizontal className='Inside'>
-                <Segment color='blue' inverted></Segment>
-                <Segment color='green' inverted></Segment>
-                <Segment color='red' inverted></Segment>
+                <div className="ui blue inverted segment Change">meow</div>
+                <Segment className="Change2" color='green' inverted></Segment>
+                <Segment className="Change3" color='red' inverted></Segment>
             </Segment.Group>
           </Grid>
         </div>
 
         <div className="Centered2">
-          <Grid className="Boxes" basic container columns={3} padded>
+          <Grid className="Boxes" container columns={3}>
             <Segment.Group horizontal className='Inside'>
-                <Segment color='orange' inverted></Segment>
-                <Segment color='yellow' inverted></Segment>
-                <Segment color='olive' inverted></Segment>
+                <Segment className="Change4" color='orange' inverted></Segment>
+                <Segment className="Change5" color='yellow' inverted></Segment>
+                <Segment className="Change6" color='black' inverted></Segment>
             </Segment.Group>
           </Grid>
         </div>
