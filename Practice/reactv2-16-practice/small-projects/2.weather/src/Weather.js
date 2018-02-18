ef119c55bbcb398488b9ea32bc40f773e5f4003d
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 21:32:54 -0700

    imported open weather package

--- Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
+++ Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import OpenWeatherMap from 'react-open-weather-map';
 
 class Weather extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            weather: 72
+            weather: 'grabbing your weather...',
+            weatherRes: ''
         };
     }
     //***********************
     componentDidMount(){
-        navigator.geolocation.getCurrentPosition((position) => console.log(position) )
+        navigator.geolocation.getCurrentPosition((position) => {
+            //coordinates
+            var lon = position.coords.longitude;
+            var lat = position.coords.latitude; 
+            var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=2c932966f6214d928f480ab04a2e75b2";
+
+            this.setState({weatherRes: api})
+        })
     }//***********************
 
     render() {
       return (
         <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {this.state.weather}.</h2>
+          <h1>{this.state.weather} {this.state.weatherRes}</h1>
         </div>
       );
     }
