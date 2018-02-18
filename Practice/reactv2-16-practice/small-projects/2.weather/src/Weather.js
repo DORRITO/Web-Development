7524cddd0171c93ae6e20f018c06cfa6d4e37ba3
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 22:37:33 -0700

    grabbed f and c

--- Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
+++ Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
@@ -1,32 +1,34 @@
 import React from 'react';
 
 class Weather extends React.Component {
+    ///////////////////////
     constructor(props) {
         super(props);
         this.state = {
             weather: 'grabbing your weather...',
             weatherRes: ''
         };
-    }
+    }///////////////////////
 
-    //***********************
+    //*********************************************************************
     componentDidMount(){
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //coordinates
-                var lon = position.coords.longitude;
-                var lat = position.coords.latitude; 
-                var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=2c932966f6214d928f480ab04a2e75b2";
-                // this.setState({weatherRes: res})
-                this.callAPI(api)
-                    .then(res => console.log(res))
-                    .catch(err => console.log(err))
-            },
-            (error) => this.setState({ weather: error.message }),
-            //timeout
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-        );
-    }//***********************
+        navigator.geolocation.getCurrentPosition((position) => {
+            //coordinates
+            var lon = position.coords.longitude;
+            var lat = position.coords.latitude; 
+            var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=2c932966f6214d928f480ab04a2e75b2";
+            // this.setState({weatherRes: res})
+            this.callAPI(api)
+                .then(res => {
+                        let kTemp = res.main.temp;
+                        let fTemp = kTemp * (9/5) - 459.67;
+                        let cTemp = kTemp - 273.15;
+                        console.log(fTemp.toFixed(1), cTemp.toFixed(1))
+                    }
+                )
+                .catch(err => console.log(err))
+        });
+    }//*********************************************************************
 
     /////////////////////////////////////////////
     callAPI = async (api) =>{
@@ -36,14 +38,14 @@ class Weather extends React.Component {
         return body;
     }/////////////////////////////////////////////
 
+    ////////////////////////////////////////////////////////////////////////////
     render() {
       return (
         <div>
           <h1>{this.state.weather}</h1>
-          <h3></h3>
         </div>
       );
-    }
+    }//////////////////////////////////////////////////////////////////////////////
   }
 
   export default Weather;
