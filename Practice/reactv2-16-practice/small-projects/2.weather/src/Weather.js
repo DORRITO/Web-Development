010f336d5a582deae9b4bf2de77ccbe3d083753e
CHIp Wright <frisbeeprod@msn.com>
2018-02-17 22:29:52 -0700

    es7 async await api call

--- Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
+++ Practice/reactv2-16-practice/small-projects/2.weather/src/Weather.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import OpenWeatherMap from 'react-open-weather-map';
 
 class Weather extends React.Component {
     constructor(props) {
@@ -9,22 +8,39 @@ class Weather extends React.Component {
             weatherRes: ''
         };
     }
+
     //***********************
     componentDidMount(){
-        navigator.geolocation.getCurrentPosition((position) => {
-            //coordinates
-            var lon = position.coords.longitude;
-            var lat = position.coords.latitude; 
-            var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=2c932966f6214d928f480ab04a2e75b2";
-
-            this.setState({weatherRes: api})
-        })
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //coordinates
+                var lon = position.coords.longitude;
+                var lat = position.coords.latitude; 
+                var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=2c932966f6214d928f480ab04a2e75b2";
+                // this.setState({weatherRes: res})
+                this.callAPI(api)
+                    .then(res => console.log(res))
+                    .catch(err => console.log(err))
+            },
+            (error) => this.setState({ weather: error.message }),
+            //timeout
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
     }//***********************
+
+    /////////////////////////////////////////////
+    callAPI = async (api) =>{
+        const response = await fetch(api);
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    }/////////////////////////////////////////////
 
     render() {
       return (
         <div>
-          <h1>{this.state.weather} {this.state.weatherRes}</h1>
+          <h1>{this.state.weather}</h1>
+          <h3></h3>
         </div>
       );
     }
