102659a9fc89372f04c200429d9bb64f839e6644
CHIp Wright <frisbeeprod@msn.com>
2018-02-19 06:14:02 -0700

    initial commit

---
+++ Practice/reactv2-16-practice/boilerplate-frontend/src/Component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { Grid, Segment } from 'semantic-ui-react';
+
+class Component extends React.Component {
+    ///////////////////////
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }///////////////////////
+
+    //*******************
+    componentDidMount(){
+        
+    }//******************
+
+    ////////////////////////////////////////////////////////////////////////////
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }//////////////////////////////////////////////////////////////////////////////
+  }
+
+  export default Component;
