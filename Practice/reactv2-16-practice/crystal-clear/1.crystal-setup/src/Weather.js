3a91e9e673f690758de8417d844e53fe81f416be
CHIp Wright <frisbeeprod@msn.com>
2018-02-19 06:08:34 -0700

    init commit

---
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/Weather.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { Grid, Segment } from 'semantic-ui-react';
+
+class Header extends React.Component {
+    ///////////////////////
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }///////////////////////
+
+    //*******************
+    componentDidMount(){
+        
+    }//******************
+
+    ////////////////////////////////////////////////////////////////////////////
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }//////////////////////////////////////////////////////////////////////////////
+  }
+
+  export default Header;
