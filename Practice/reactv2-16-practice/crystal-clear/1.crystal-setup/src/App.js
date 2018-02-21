6efa358dba13226b70caade45e63745dd987a260
CHIp Wright <frisbeeprod@msn.com>
2018-02-20 20:29:49 -0700

    added components, and top picture

--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/App.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/App.js
@@ -6,8 +6,7 @@ import Header from './components/Header';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-      asdasdasd
+      <div>
         <Header />
       </div>
     );
--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
@@ -18,7 +18,7 @@ class Header extends React.Component {
     ////////////////////////////////////////////////////////////////////////////
     render() {
       return (
-        <div>
+        <div className='Header'>
           JABAWAAKKIIII
         </div>
       );
