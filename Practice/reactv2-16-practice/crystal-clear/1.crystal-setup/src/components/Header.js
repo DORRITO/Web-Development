5b6edecc83d29be7d6214b978bb5b3cd1308182c
CHIp Wright <frisbeeprod@msn.com>
2018-02-21 01:09:49 -0700

    added more components, removed main. adjusted header

--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
+// import { Grid, Segment } from 'semantic-ui-react';
 
 class Header extends React.Component {
-    ///////////////////////
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        };
-    }///////////////////////
-
-    //*******************
-    componentDidMount(){
-        
-    }//******************
-
-    ////////////////////////////////////////////////////////////////////////////
-    render() {
-      return (
-        <div className='Header'>
-          <h1 className="Text">Crystal</h1>
-        </div>
-      );
-    }//////////////////////////////////////////////////////////////////////////////
+    
+  render() {
+    return (
+      <div className='Header'>
+      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
+      <h1 className="Text">Crystal</h1>
+      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
+      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
+      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
+      <h1 className="Text">Clear</h1>
+      </div>
+    );
   }
+}
 
   export default Header;
---
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Grid, Segment } from 'semantic-ui-react';
+
+import React, { Component } from 'react';
+
+class Servies extends Component {
+  render() {
+    return (
+      <div className="Services" >
+      </div>
+    );
+  }
+}
+
+export default Servies;
