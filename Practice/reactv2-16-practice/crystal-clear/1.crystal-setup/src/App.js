4c2fade3ae7f43b2b907176326e095012c4b8a66
CHIp Wright <frisbeeprod@msn.com>
2018-02-21 14:36:22 -0700

    set headline text, services, and contact

--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/App.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './components/Header';
+import Title from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Title />
       </div>
     );
   }
--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
-// import { Grid, Segment } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
 
-class Header extends React.Component {
+class Title extends React.Component {
     
   render() {
     return (
-      <div className='Header'>
-      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
-      <h1 className="Text">Crystal</h1>
+      <div className='Title'>
+      <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
+      <Header style={{margin: '2rem 0 0 25%', display: 'inline-block'}} dividing floated='left'>Services</Header>
+      <Header style={{margin: '2rem 25% 0 0', display: 'inline-block'}} dividing floated='right'>Contact</Header>
+      <h1 className="Text1">Crystal</h1>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
-      <h1 className="Text">Clear</h1>
+      <h1 className="Text2">Clear</h1>
       </div>
     );
   }
 }
 
-  export default Header;
+  export default Title;
