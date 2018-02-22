aa4d3e503b720ac2c58003cd52781b917125e573
CHIp Wright <frisbeeprod@msn.com>
2018-02-22 01:12:31 -0700

    text in the middle of clients

--- Practice/reactv2-16-practice/crystal-clear/2.crystal-styling/src/components/Clients.js
+++ Practice/reactv2-16-practice/crystal-clear/2.crystal-styling/src/components/Clients.js
@@ -6,12 +6,12 @@ class Clients extends Component {
   render() {
     return (
       <div className="Clients" >
-        <Segment style={{marginTop: '1rem'}} >
+        <Segment style={{marginTop: '1rem'}} className="Text-Services">
             <div className="Commercial" style={{marginLeft: '10%'}}>
-                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block'}} floated='centered'>Commercial</Header>
+                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block', fontFamily: 'Playball', fontSize: '300%'}} floated='centered'>Commercial</Header>
             </div>
             <div className="Residential" style={{marginLeft: '15%'}}>
-                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block'}} floated='center'>Residential</Header>
+                <Header style={{margin: '2rem 0 25% 0', display: 'inline-block', fontFamily: 'Playball', fontSize: '300%', webkitTextStroke: '.1rem #000'}} floated='center'>Residential</Header>
             </div>
         </Segment>
       </div>
