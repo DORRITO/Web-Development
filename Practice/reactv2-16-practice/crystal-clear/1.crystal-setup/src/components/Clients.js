23b8da39c6913eadfaf929c34d06f7c05e768ef1
CHIp Wright <frisbeeprod@msn.com>
2018-02-21 19:50:15 -0700

    services set up

--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Clients.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Clients.js
@@ -8,7 +8,7 @@ class Clients extends Component {
       <div className="Clients" >
         <Segment style={{marginTop: '1rem'}}>
             <div className="Commercial">
-            <Header style={{margin: '2rem 0 0 25%', display: 'inline-block'}} floated='left'>Commercial</Header>
+                <Header style={{margin: '2rem 0 0 25%', display: 'inline-block'}} floated='left'>Commercial</Header>
             </div>
             <div className="Residential">
                 <Header style={{margin: '2rem 25% 0 0', display: 'inline-block'}} floated='right'>Residential</Header>
--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Services.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Services.js
@@ -6,13 +6,31 @@ class Servies extends Component {
     return (
       <div className="Services" >
         <Segment size='large' inverted style={{backgroundColor: '#E5E5DB'}}>
-          <Grid columns={3} padded >
-            <Grid.Column color='blue'>Maintenance</Grid.Column>
-            <Grid.Column color='blue'>Cleaning</Grid.Column>
-            <Grid.Column color='blue'>Supplies</Grid.Column>
-            <Grid.Column color='blue'>Maintenance</Grid.Column>
-            <Grid.Column color='blue'>Cleaning</Grid.Column>
-            <Grid.Column color='blue'>Supplies</Grid.Column>
+          <Grid columns={3}>
+            <Grid.Column><Icon size='large' color='bue' inverted name='wrench' />
+              Maintenance
+              <h6>meow</h6>
+            </Grid.Column>
+            <Grid.Column><Icon size='large' color='bue' inverted name='leaf' />
+              Cleaning
+              <h6>meow</h6>
+            </Grid.Column>
+            <Grid.Column><Icon size='large' color='bue' inverted name='archive' />
+              Supplies
+              <h6>meow</h6>
+            </Grid.Column>
+            <Grid.Column><Icon size='large' color='bue' inverted name='first aid' />
+              Restoration
+              <h6>meow</h6>
+            </Grid.Column>
+            <Grid.Column><Icon size='large' color='bue' inverted name='tint' />
+              Chemicals
+              <h6>meow</h6>
+            </Grid.Column>
+            <Grid.Column><Icon size='large' color='bue' inverted name='child' />
+              Customer Service
+              <h6>meow</h6>
+            </Grid.Column>
           </Grid>
         </Segment>
       </div>
