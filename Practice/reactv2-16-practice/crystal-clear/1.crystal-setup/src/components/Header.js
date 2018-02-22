8a0722a5a0f892a3fc4a6e4e02de85d5a96e9e71
CHIp Wright <frisbeeprod@msn.com>
2018-02-21 17:36:09 -0700

    added scroll functionality to be edited later

--- Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
+++ Practice/reactv2-16-practice/crystal-clear/1.crystal-setup/src/components/Header.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import { Header, Segment } from 'semantic-ui-react';
 
+import * as Scroll from 'react-scroll';
+import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
 class Title extends React.Component {
     
+  componentDidMount(){
+    Events.scrollEvent.register('begin', function(to, element) {
+      console.log("begin", arguments);
+    });
+ 
+    Events.scrollEvent.register('end', function(to, element) {
+      console.log("end", arguments);
+    });
+ 
+    scrollSpy.update();
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+  scrollToBottom() {
+    scroll.scrollToBottom();
+  }
+  scrollTo() {
+    scroll.scrollTo(100);
+  }
+  scrollMore() {
+    scroll.scrollMore(100);
+  }
+  handleSetActive(to) {
+    console.log(to);
+  }
+
   render() {
     return (
       <div className='Title'>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
-      <Header style={{margin: '2rem 0 0 25%', display: 'inline-block'}} dividing floated='left'>Services</Header>
-      <Header style={{margin: '2rem 25% 0 0', display: 'inline-block'}} dividing floated='right'>Contact</Header>
+      <a onClick={this.scrollToBottom}>
+        <Header style={{margin: '2rem 0 0 25%', display: 'inline-block'}} dividing floated='left'>Services</Header>
+      </a>
+      <a onClick={this.scrollToBottom}>
+        <Header style={{margin: '2rem 25% 0 0', display: 'inline-block'}} dividing floated='right'>Contact</Header>
+      </a>
       <h1 className="Text1">Crystal</h1>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
       <h1>{null}</h1><h1>{null}</h1><h1>{null}</h1><h1>{null}</h1>
