f7b54303545460a220022a2b34fea2d1764f9b84
CHIp Wright <frisbeeprod@msn.com>
2018-02-24 21:32:25 -0700

    added passport and oauth passport

--- Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
+++ Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
