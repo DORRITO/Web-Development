00c18d9f0001a60d9943b755322833dc0874bf1f
CHIp Wright <frisbeeprod@msn.com>
2018-02-26 04:18:30 -0700

    added mongoose and set its connection in config

--- Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
+++ Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require("./services/passport");
+
+// mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
