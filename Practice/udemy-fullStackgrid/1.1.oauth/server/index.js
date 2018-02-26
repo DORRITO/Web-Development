04a0aab27b7e168542ebe69cf9aec03cb4ba9eee
CHIp Wright <frisbeeprod@msn.com>
2018-02-26 06:44:37 -0700

    added cookie-session.  finished token authentication

--- Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
+++ Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
@@ -8,6 +10,15 @@ require("./services/passport");
 mongoose.connect(process.env.MONGODB_URI || keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //days, hours, minutes, seconds, milliseconds
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
