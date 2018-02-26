0f92a6948d5da6c4f74871d2620320724ce18743
CHIp Wright <frisbeeprod@msn.com>
2018-02-26 05:24:12 -0700

    set up user model and schema

--- Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
+++ Practice/udemy-fullStackgrid/1.1.oauth/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require("./services/passport");
+require("./models/User");
 
 // mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || keys.mongoURI);
