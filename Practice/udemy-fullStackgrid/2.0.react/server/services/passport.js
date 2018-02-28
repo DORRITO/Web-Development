47178f8b6ab598699030234359462ad504c2f7b1
CHIp Wright <frisbeeprod@msn.com>
2018-02-27 23:27:38 -0700

    refactored passport to use async/await

--- Practice/udemy-fullStackgrid/2.0.react/server/services/passport.js
+++ Practice/udemy-fullStackgrid/2.0.react/server/services/passport.js
@@ -21,18 +21,16 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      proxy : true
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      }).catch(err => console.log(err));
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
