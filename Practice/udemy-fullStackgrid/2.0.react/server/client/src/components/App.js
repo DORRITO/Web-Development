d6555a75b7066ba9a8017aae62e200f723e5f7df
CHIp Wright <frisbeeprod@msn.com>
2018-02-28 03:55:02 -0700

    changed app to a class component

--- Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/App.js
+++ Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./Header";
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+
+    componentDidMount(){
+        
+    }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
