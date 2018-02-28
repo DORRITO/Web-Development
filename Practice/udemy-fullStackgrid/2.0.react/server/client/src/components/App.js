7a8fff05b388df1e99220890fe02c57c5cd89deb
CHIp Wright <frisbeeprod@msn.com>
2018-02-28 01:48:59 -0700

    added header and install materialize-css

--- Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/App.js
+++ Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
 
 const App = () => {
-    return(
+  return (
+    <div>
+      <BrowserRouter>
         <div>
-            Herro!
+          <Header />
         </div>
-    );
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
