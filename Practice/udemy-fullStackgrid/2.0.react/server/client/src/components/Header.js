e1f0f5be0deed426922f0596908ca3d592b97987
CHIp Wright <frisbeeprod@msn.com>
2018-02-28 19:15:48 -0700

    added redux mapstate to header, and fluid message depending on state

--- Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/Header.js
+++ Practice/udemy-fullStackgrid/2.0.react/server/client/src/components/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 
 class Header extends Component {
+
+  renderContent(){
+    switch(this.props.auth){
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'im logged out';
+      default:
+       return 'im logged in';
+    }
+  }
+
   render() {
+    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
           <ul className="right">
-            <a>Login with Google</a>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({auth}) => {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
