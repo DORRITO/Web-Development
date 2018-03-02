2b97cbd45de8fb7e7efea0391bff444dcb5be3d0
CHIp Wright <frisbeeprod@msn.com>
2018-03-02 03:50:01 -0700

    added custom button to payments, added keys to header li

--- Practice/udemy-fullStackgrid/3.0.stripe-card/server/client/src/components/Payments.js
+++ Practice/udemy-fullStackgrid/3.0.stripe-card/server/client/src/components/Payments.js
@@ -1,16 +1,20 @@
-import React, {Component} from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-class Payments extends Component{
-    render(){
-        return(
-            <StripeCheckout 
-                amount={500}
-                token={token => console.log(token)}
-                stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
-        );
-    }
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="email"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
 }
 
 export default Payments;
