73e9aac6e4f6e7e7bb0edc8bed7e944ce72004e6
CHIp Wright <frisbeeprod@msn.com>
2018-03-14 22:22:55 -0700

    switched order of if statement, cleaned up code

--- Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
+++ Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
@@ -22,14 +22,13 @@ while (isSunk === false) {
   if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
     alert("HIT!");
+    //sunk?
+    if (hits == 3) {
+      isSunk = true;
+      alert("You sank my battleship!");
+    }
   } else {
     alert("MISS!");
-  }
-
-  //sunk?
-  if (hits == 3) {
-    isSunk = true;
-    alert("You sank my battleship!");
   }
 }
 
