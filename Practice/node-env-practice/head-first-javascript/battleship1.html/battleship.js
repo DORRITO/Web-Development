1232d2e810c4f3d65ad7cead06d9f73188f4ce51
CHIp Wright <frisbeeprod@msn.com>
2018-03-14 22:18:27 -0700

    added post game stats

--- Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
+++ Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
@@ -29,3 +29,7 @@ while (isSunk === false) {
     alert("You sank my battleship!");
   }
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+  "which means your shooting accuracy was " +3 / guesses;
+alert(stats);
