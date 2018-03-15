3411091da4eab41f77c11e235c4e554b37194301
CHIp Wright <frisbeeprod@msn.com>
2018-03-14 22:15:05 -0700

    added check for sinking

--- Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
+++ Practice/node-env-practice/head-first-javascript/battleship1.html/battleship.js
@@ -22,4 +22,10 @@ while (isSunk === false) {
   if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
   }
+
+  //sunk?
+  if (hits == 3) {
+    isSunk = true;
+    alert("You sank my battleship!");
+  }
 }
