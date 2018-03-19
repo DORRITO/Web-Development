515e48bfac119e6624885c993fbfe2905ef3e674
CHIp Wright <frisbeeprod@msn.com>
2018-03-19 02:04:09 -0700

    remove comment

--- Practice/node-env-practice/algo/AlgoCasts/exercises/anagrams/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/anagrams/index.js
@@ -10,7 +10,7 @@
 
 function anagrams(stringA, stringB) {
   const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
+  const bCharMap = buildCharMap(stringBa);
 
   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
     return false;
@@ -36,30 +36,3 @@ function buildCharMap(str) {
 }
 
 module.exports = anagrams;
-
-// function anagrams(stringA, stringB) {
-//   const aCharMap = buildCharMap(stringA);
-//   const bCharMap = buildCharMap(stringB);
-//
-//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-//     return false;
-//   }
-//
-//   for (let char in aCharMap) {
-//     if (aCharMap[char] !== bCharMap[char]) {
-//       return false;
-//     }
-//   }
-//
-//   return true;
-// }
-//
-// function buildCharMap(str) {
-//   const charMap = {};
-//
-//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-//     charMap[char] = charMap[char] + 1 || 1;
-//   }
-//
-//   return charMap;
-// }
