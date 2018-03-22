9210e43dd54fc76d39baf86c8b4c1df08e140b54
CHIp Wright <frisbeeprod@msn.com>
2018-03-22 01:21:18 -0700

    return char

--- Practice/node-env-practice/algo/AlgoCasts/exercises/maxchar/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/maxchar/index.js
@@ -6,21 +6,7 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    let charList = {}
-    let mostUsed = '';
-    let num = ''
-
-    for (let i of str){
-        charList.i += 1
-    }
-
-    for (let i in charList){
-        if(i > num){
-            num = i;
-            mostUsed = charList.i
-        }
-    }
-    return mostUsed;
+    
 }
 
 module.exports = maxChar;
