01ecca592286d1aafdfb8a2532adb12f74feefe2
CHIp Wright <frisbeeprod@msn.com>
2018-04-11 19:02:28 -0700

    phase out queue

--- Practice/node-env-practice/algo/AlgoCasts/exercises/qfroms/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/qfroms/index.js
@@ -15,42 +15,42 @@
 const Stack = require('./stack');
 
 class Queue {
-    constructor(){
-        this.first = new Stack();
-        this.second = new Stack();
-    }
+    // constructor(){
+    //     this.first = new Stack();
+    //     this.second = new Stack();
+    // }
 
-    add(record){
-        this.first.push(record);
-    }
+    // add(record){
+    //     this.first.push(record);
+    // }
 
-    remove(){
-        while(this.first.peek()) {
-            this.second.push(this.first.pop());
-        }
+    // remove(){
+    //     while(this.first.peek()) {
+    //         this.second.push(this.first.pop());
+    //     }
 
-        const record = this.second.pop();
+    //     const record = this.second.pop();
 
-        while(this.second.peek()){
-            this.first.push(this.second.pop());
-        }
+    //     while(this.second.peek()){
+    //         this.first.push(this.second.pop());
+    //     }
 
-        return record;
-    }
+    //     return record;
+    // }
 
-    peek(){
-       while(this.first.peek()){
-           this.second.push(this.first.pop());
-       }
+    // peek(){
+    //    while(this.first.peek()){
+    //        this.second.push(this.first.pop());
+    //    }
 
-       const record = this.second.peek();
+    //    const record = this.second.peek();
 
-       while (this.second.peek()){
-           this.first.push(this.second.pop());
-       }
+    //    while (this.second.peek()){
+    //        this.first.push(this.second.pop());
+    //    }
 
-       return record;
-    }
+    //    return record;
+    // }
 }
 
 module.exports = Queue;
