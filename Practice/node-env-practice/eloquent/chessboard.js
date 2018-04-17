4402abc387c387a3607db90b40b380885f13c55f
CHIp Wright <frisbeeprod@msn.com>
2018-04-17 16:04:07 -0700

    ternary version

--- Practice/node-env-practice/eloquent/chessboard.js
+++ Practice/node-env-practice/eloquent/chessboard.js
@@ -1,13 +1,9 @@
 const size = 8;
 let board = "";
 
-for (let i = 1; i <= size; i++) {
+for (let i = 1; i < size; i++) {
   for (let j = 1; j < size; j++) {
-    if ((i + j) % 2 === 0) {
-      board += " ";
-    } else {
-      board += "#";
-    }
+    (j + i) % 2 === 0 ? (board += " ") : (board += "#");
   }
   board += "\n";
 }
