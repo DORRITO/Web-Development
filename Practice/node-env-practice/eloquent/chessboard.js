afc93ede660f6ab8d2ff0b570636cd1d5f2efc21
CHIp Wright <frisbeeprod@msn.com>
2018-04-17 00:08:05 -0700

    chess

---
+++ Practice/node-env-practice/eloquent/chessboard.js
@@ -0,0 +1,15 @@
+const size = 8;
+let board = "";
+
+for (let i = 1; i <= size; i++) {
+  for (let j = 1; j < size; j++) {
+    if ((i + j) % 2 === 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
