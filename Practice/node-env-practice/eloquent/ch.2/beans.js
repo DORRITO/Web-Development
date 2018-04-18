6a2b674e176d96f1b9481746a27ba3c19ea6b8f1
CHIp Wright <frisbeeprod@msn.com>
2018-04-17 23:11:30 -0700

    beans + first function

---
+++ Practice/node-env-practice/eloquent/ch.2/beans.js
@@ -0,0 +1,13 @@
+const countBs = (word) => {
+    let count = 0;
+    for (let i of word){
+        if (i === "B") count += 1;
+    }
+    console.log(count)
+}
+
+countBs("BBCA")
+
+const countChar = (word, char) => {
+
+}
