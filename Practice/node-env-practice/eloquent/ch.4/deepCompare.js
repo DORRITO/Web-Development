7f98329b733a6134d7aadea7c7ccf3afe5611d9e
CHIp Wright <frisbeeprod@msn.com>
2018-04-19 02:21:19 -0700

    init commit + functions

---
+++ Practice/node-env-practice/eloquent/ch.4/deepCompare.js
@@ -0,0 +1,26 @@
+const deepEqual = (val1, val2) => {
+  if (val1 === val2) {
+    if (val1 === null || val2 === null) {
+      return false;
+    }
+    return true;
+  } else if (typeof val1 === "object" && typeof val2 === "object") {
+    if (val1 === null || val2 === null) {
+      return false;
+    }
+    for (let i in val1) {
+      if (val1[i] !== val2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const a = {a: 1, b: 2};
+const b = {a: 1, b: 2};
+
+console.log(deepEqual(a, b))
