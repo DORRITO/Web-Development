216b9c629bd80fc4731d34c212d4110f92b484cd
CHIp Wright <frisbeeprod@msn.com>
2018-04-18 23:53:42 -0700

    change current arr

--- Practice/node-env-practice/eloquent/ch.4/revarr.js
+++ Practice/node-env-practice/eloquent/ch.4/revarr.js
@@ -6,4 +6,14 @@ const reverseArray = arr => {
     return newArr;
 }
 
+const reverseArrayInPlace = arr => {
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        let old = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = old;
+      }
+    return arr;
+}
+
 console.log(reverseArray(['hi', 'bye', 'wut']));
+console.log(reverseArrayInPlace(['hi', 'bye', 'wut']));
