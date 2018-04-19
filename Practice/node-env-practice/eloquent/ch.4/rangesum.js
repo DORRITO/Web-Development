439222b9753fd939e81b8282a8d5f7359f74355d
CHIp Wright <frisbeeprod@msn.com>
2018-04-18 23:41:25 -0700

    prettified the code, added comments

--- Practice/node-env-practice/eloquent/ch.4/rangesum.js
+++ Practice/node-env-practice/eloquent/ch.4/rangesum.js
@@ -1,17 +1,21 @@
-const range = (start, end, step = 1) => {
-    rangeArr = [];
-    for (let i = start; i <= end; i += step) {
-        rangeArr.push(i);
-    }
-    return rangeArr;
-}
+//takes a range of numbers and adds them all up.
 
+//specifies the range, skipping steps is optional
+const range = (start, end, step = 1) => {
+  rangeArr = [];
+  for (let i = start; i <= end; i += step) {
+    rangeArr.push(i);
+  }
+  return rangeArr;
+};
+
+//add the range array up
 const sum = numArr => {
-    let total = 0;
-    for (let i of numArr){
-        total += i;
-    }
-    return total;
-}
+  let total = 0;
+  for (let i of numArr) {
+    total += i;
+  }
+  return total;
+};
 
 console.log(sum(range(1, 10, 2)));
