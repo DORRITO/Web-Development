26f7d93c4465207a6939584a6a77a6cdf2d730fd
CHIp Wright <frisbeeprod@msn.com>
2018-04-19 23:52:47 -0700

    temporary ui button

--- Practice/reactv2-16-practice/second-violin/src/App.js
+++ Practice/reactv2-16-practice/second-violin/src/App.js
@@ -5,9 +5,14 @@ import { Grid, Header } from 'semantic-ui-react';
 class App extends Component {
   render() {
     return (
+        <div>
         <Header as='h2' className="App-header" textAlign='center' verticalAlign='middle'>
-          Welcome to Second Violin
-        </Header>
+        Welcome to Second Violin
+      </Header>
+      <button className="ui button">
+        Button
+      </button>
+        </div>
     );
   }
 }
