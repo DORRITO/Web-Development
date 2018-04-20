6926af7776634d0fb88c36c0df969a8434ffd56e
CHIp Wright <frisbeeprod@msn.com>
2018-04-19 23:49:57 -0700

    messing with grid and header

--- Practice/reactv2-16-practice/second-violin/src/App.js
+++ Practice/reactv2-16-practice/second-violin/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Header } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-        <Header as='h2' className="App-header">
-          <div className="App-title">Welcome to Second Violin</div>
+        <Header as='h2' className="App-header" textAlign='center' verticalAlign='middle'>
+          Welcome to Second Violin
         </Header>
     );
   }
