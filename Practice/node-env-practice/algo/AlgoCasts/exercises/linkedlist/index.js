21c3bc2b7040c6aad8be286d7798d4bb85f4353c
CHIp Wright <frisbeeprod@msn.com>
2018-04-20 03:06:17 -0700

    insert first

--- Practice/node-env-practice/algo/AlgoCasts/exercises/linkedlist/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/linkedlist/index.js
@@ -3,15 +3,20 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next){
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
 }
-
 
 module.exports = { Node, LinkedList };
