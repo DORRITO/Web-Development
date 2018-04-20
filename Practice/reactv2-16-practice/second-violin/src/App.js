8bfd099c06874ca9923f6ff0829992ef5a9404cb
CHIp Wright <frisbeeprod@msn.com>
2018-04-19 23:28:15 -0700

    added/removed styles, set up welcome message

--- Practice/reactv2-16-practice/second-violin/src/App.js
+++ Practice/reactv2-16-practice/second-violin/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Second Violin</h1>
-        </header>
-      </div>
+        <Header as='h2' className="App-header">
+          <div className="App-title">Welcome to Second Violin</div>
+        </Header>
     );
   }
 }
