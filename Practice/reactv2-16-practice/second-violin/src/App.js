610ed25a8f1cefa1fa478a324c655298c3f7f61e
CHIp Wright <frisbeeprod@msn.com>
2018-04-21 21:19:27 -0700

    page code format

--- Practice/reactv2-16-practice/second-violin/src/App.js
+++ Practice/reactv2-16-practice/second-violin/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Grid, Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import "./App.css";
+import { Grid, Header } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-        <div>
-        <Header as='h2' className="App-header" textAlign='center' verticalAlign='middle'>
-        Welcome to Second Violin
-      </Header>
-      <button className="ui button">
-        Button
-      </button>
-        </div>
+      <div>
+        <Header as="h2" className="App-header" textAlign="center" verticalAlign="middle">
+          Welcome to Second Violin
+        </Header>
+        <button className="ui button">Button</button>
+      </div>
     );
   }
 }
