5765352521eb25e5d5326bc0f7ba3b6427f4d7f3
CHIp Wright <frisbeeprod@msn.com>
2018-04-21 21:04:28 -0700

    router

---
+++ Practice/reactv2-16-practice/second-violin/src/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router, Route, Switch} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import App from '../components/App';
+
+export const history = createHistory();
+
+const AppRouter = () => (
+    <Router history={history}>
+        <Switch>
+            <Route path="/" component={App} exact={true} />
+        </Switch>
+    </Router>
+);
