be4c15e9f672cec090635b5f1069ca09170c758f
CHIp Wright <frisbeeprod@msn.com>
2018-04-21 21:11:43 -0700

    exported router, changed index files (currently broken

--- Practice/reactv2-16-practice/second-violin/src/index.js
+++ Practice/reactv2-16-practice/second-violin/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppRouter from "./AppRouter";
+import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 registerServiceWorker();
