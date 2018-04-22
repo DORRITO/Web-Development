976809052518ede87dd989cdcf87adc3b89d701d
CHIp Wright <frisbeeprod@msn.com>
2018-04-22 15:43:36 -0700

    added header to segment

--- Practice/reactv2-16-practice/second-violin/src/App.js
+++ Practice/reactv2-16-practice/second-violin/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Header } from "semantic-ui-react";
+import { Header, Segment } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header as="h2" className="App-header" textAlign="center" verticalalign="middle">
-          <div className="App-title">Welcome to Second Violin</div>
-        </Header>
+        <Segment raised color='teal'>
+          <Header as="h2" className="App-header" textAlign="center" verticalalign="middle">
+            <div className="App-title">Welcome to Second Violin</div>
+          </Header>
+        </Segment>
       </div>
     );
   }
