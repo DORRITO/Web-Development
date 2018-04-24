bdeaf0c6faef2d657ff5c06f1815c08e90f005d8
CHIp Wright <frisbeeprod@msn.com>
2018-04-23 23:56:46 -0700

    added components folder and songs component

---
+++ Practice/reactv2-16-practice/second-violin/src/components/Songs.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Songs extends Component {
+    render() {
+      return (
+        <div>
+          This is the song component
+        </div>
+      );
+    }
+  }
+  
+  export default Songs;
