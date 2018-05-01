00cfdbd945acef111b204e66cb1f3e44161bb9c6
CHIp Wright <frisbeeprod@msn.com>
2018-05-01 01:34:30 -0700

    breadth first

--- Practice/node-env-practice/algo/AlgoCasts/exercises/tree/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/tree/index.js
@@ -28,9 +28,19 @@ class Node {
 }
 
 class Tree {
-    constructor(){
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    let arr = [this.root];
+    while (arr.length) {
+        const node = arr.shift();
+
+        arr.push(...node.children)
+        fn(node);
     }
+  }
 }
 
 module.exports = { Tree, Node };
