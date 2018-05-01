6b84e94d85cfcac6d5a53f43201d02c2569ea9b3
CHIp Wright <frisbeeprod@msn.com>
2018-04-30 22:21:27 -0700

    added country, made title 1/3rd smaller

---
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Country.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Country extends Component{
+    render(){
+        return(
+            <div>Country</div>
+        )
+    }
+}
+
+export default Country;
