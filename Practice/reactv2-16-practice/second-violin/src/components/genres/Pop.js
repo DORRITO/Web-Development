2796d7aee07cfe6c334a15f6634ca4abf91879f8
CHIp Wright <frisbeeprod@msn.com>
2018-05-01 23:37:26 -0700

    pop component

---
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Pop.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Pop extends Component{
+    render(){
+        return(
+            <div>
+                Pop component!
+            </div>
+        )
+    }
+}
+
+export default Pop;
