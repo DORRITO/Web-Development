2fdf4562eb3a83efd4e758f2b245ef69fc5a2272
CHIp Wright <frisbeeprod@msn.com>
2018-05-01 23:56:51 -0700

    rap component

---
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Rap.js
@@ -0,0 +1,11 @@
+import React, {Component} from react;
+
+class Rap extends Component{
+    render(){
+        return(
+            <div>Rap component</div>
+        )
+    }
+}
+
+export default Rap;
