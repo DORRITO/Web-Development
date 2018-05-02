cf7f1bef075b1fc09bf4317edcdf72b1f8318cc8
CHIp Wright <frisbeeprod@msn.com>
2018-05-01 23:53:21 -0700

    blues component

---
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Blues.js
@@ -0,0 +1,11 @@
+import React, {Component} from react;
+
+class Blues extends Component{
+    render(){
+        return(
+            <div>Blues component</div>
+        )
+    }
+}
+
+export default Blues;
