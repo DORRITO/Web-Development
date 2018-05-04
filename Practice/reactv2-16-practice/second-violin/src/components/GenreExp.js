929c77079a068e2e39d278d9ecde4022945ca5ac
CHIp Wright <frisbeeprod@msn.com>
2018-05-03 23:58:33 -0700

    added genre explanation

---
+++ Practice/reactv2-16-practice/second-violin/src/components/GenreExp.js
@@ -0,0 +1,15 @@
+//quick info for each page that says why there aren't a million sub genres.  Exp = explanation
+import React, {Component} from "react";
+
+class GenreExp extends Component{
+    render(){
+        return(
+            <div>
+                <p>Remove me from main page!</p>
+                There are only 5 genres because bla bla bla.....<br />
+            </div>
+        )
+    }
+}
+
+export default GenreExp;
