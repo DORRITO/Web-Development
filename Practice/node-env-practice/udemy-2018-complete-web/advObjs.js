5e5421569877c71e77e72988aeddfad6ffd9a79e
CHIp Wright <frisbeeprod@msn.com>
2018-05-06 00:22:16 -0700

    classes

--- Practice/node-env-practice/udemy-2018-complete-web/advObjs.js
+++ Practice/node-env-practice/udemy-2018-complete-web/advObjs.js
@@ -1,7 +1,7 @@
 //Evaluate these:
 //#1
 [2] === [2]  //false
-{} === {}  // false
+// {} === {}  // false
 
 //#2 what is the value of property a for each object.  <--- final property
 const object1 = { a: 5 };   //4
@@ -13,3 +13,22 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal{
+    constructor(name, type, color){
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+}
+
+class Mamal extends Animal{
+    constructor(name, type, color){
+        super(name, type, color)
+    }
+
+    sound(){console.log(this.name, this.type, this.color)};
+}
+
+const cat = new Mamal('cat', 'bengal', 'rosette');
+
+cat.sound()
