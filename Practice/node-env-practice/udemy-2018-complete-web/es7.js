3cfe01bfb3c566fe4ffacc10ceb52018501c594a
CHIp Wright <frisbeeprod@msn.com>
2018-05-07 03:34:32 -0700

    powers with **

--- Practice/node-env-practice/udemy-2018-complete-web/es7.js
+++ Practice/node-env-practice/udemy-2018-complete-web/es7.js
@@ -2,8 +2,8 @@
 
 
 // #1) Check if this array includes the name "John".
-const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-dragons.includes('John');
+const dragons1 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragons1.includes('John');
 
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
@@ -11,10 +11,10 @@ const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
 console.log(dragons.filter(name => name.includes('John')) );
 
-
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
+const powerUp = (num) => num**100;
+console.log(powerUp(5));
 
-
-// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// #4) Using your function from #3, put in the paramter 10000. What is the result?
 // Research for yourself why you get this result
-
+console.log(powerUp(10000));
