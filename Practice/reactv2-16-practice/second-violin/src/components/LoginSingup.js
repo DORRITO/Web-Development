e6a67540580ae37ef2a5c01c10568fffd136248c
CHIp Wright <frisbeeprod@msn.com>
2018-05-07 03:54:49 -0700

    created basic login component

---
+++ Practice/reactv2-16-practice/second-violin/src/components/LoginSingup.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class LoginSignup extends Component {
+  // constructor(){
+  //     super()
+  //     this.state = {
+  //         hi: 'test'
+  //     }
+  // }
+
+  render() {
+    return <div>'Login, signup form'</div>;
+  }
+}
+
+export default LoginSignup;
