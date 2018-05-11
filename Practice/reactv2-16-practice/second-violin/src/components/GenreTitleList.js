76b08b80b5bc5a10a2c4ba9879a2298eb1404426
CHIp Wright <frisbeeprod@msn.com>
2018-05-10 23:40:56 -0700

    removed component checks

--- Practice/reactv2-16-practice/second-violin/src/components/GenreTitleList.js
+++ Practice/reactv2-16-practice/second-violin/src/components/GenreTitleList.js
@@ -6,21 +6,19 @@ import Pop from './genres/Pop';
 import Rap from './genres/Rap';
 import Rock from './genres/Rock';
 
-class GenreTitleList extends Component {
+class GenreList extends Component {
     render(){
         return(
             <div>
-                genres: hi
+                genres:
                 <span>Blues </span>
                 <span>Country </span>
                 <span>Pop </span>
                 <span>Rap </span>
                 <span>Rock </span>
-                <div>Checks to make sure components are working:</div>
-                <Blues /><Country /><Pop /><Rap /><Rock />
             </div>
         )
     }
 }
 
-export default GenreTitleList;
+export default GenreList;
