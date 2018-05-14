1a74de68215b5adf07cb05182c453a86698aa3ba
CHIp Wright <frisbeeprod@msn.com>
2018-05-14 03:43:29 -0700

    added an h2 and another div

--- Practice/node-env-practice/udemy-2018-complete-web/robofriends/src/Hello.js
+++ Practice/node-env-practice/udemy-2018-complete-web/robofriends/src/Hello.js
@@ -1,9 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class Hello extends Component{
-    render(){
-        return <h1>Hello World</h1>
-    }
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Welcome to a react file</h2>
+      </div>
+    );
+  }
 }
 
 export default Hello;
