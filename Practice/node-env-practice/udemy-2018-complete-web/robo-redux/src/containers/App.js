d539b2497b94af2e5329f3a2d4fb0fe1a58a7efe
CHIp Wright <frisbeeprod@msn.com>
2018-05-16 23:28:54 -0700

    hooked up reducer state to app

--- Practice/node-env-practice/udemy-2018-complete-web/robo-redux/src/containers/App.js
+++ Practice/node-env-practice/udemy-2018-complete-web/robo-redux/src/containers/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 
 import CardList from "./../components/CardList";
 import SearchBox from "./../components/SearchBox";
 import Scroll from './../components/Scroll';
 import './App.css';
 
+import {setSearchField} from './../actions';
+
+const mapStateToProps = state => {
+  return{
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-        robots: [],
-        searchField: ''
+        robots: []
     };
   }
 
@@ -20,13 +34,9 @@ class App extends Component {
         .then(users => this.setState({robots: users}));
   }
 
-  onSearchChange = (e) => {
-    //   e.preventDefault();
-    this.setState({ searchField: e.target.value})
-  }
-
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const {searchField, onSearchChange} = this.props;
     const filterRobots = robots.filter((robot) => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
@@ -34,7 +44,7 @@ class App extends Component {
     <h1>Loading...</h1> :
     (<div className="tc">
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
             <CardList robots={filterRobots} />
         </Scroll>
@@ -42,4 +52,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
