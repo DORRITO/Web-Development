769e469694b46a7f4229cf94df5b44453588273f
CHIp Wright <frisbeeprod@msn.com>
2018-05-17 23:57:28 -0700

    cleaned up styling of component

--- Practice/node-env-practice/algo/AlgoCasts/exercises/fib/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/fib/index.js
@@ -23,9 +23,9 @@
 // }
 
 function fib(n) {
-  if(n < 2){return n}
+  // if(n < 2){return n}
 
-  return fib(n - 1) + fib(n - 2)
+  // return fib(n - 1) + fib(n -2)
 }
 
 // const fib = memoize(slowFib);
