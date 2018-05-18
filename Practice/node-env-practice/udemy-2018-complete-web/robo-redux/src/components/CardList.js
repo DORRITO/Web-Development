be07ddc82a5d37e3e8db41375bb64cb36e1fcca7
CHIp Wright <frisbeeprod@msn.com>
2018-05-17 23:58:17 -0700

    cleaned up styling of component

--- Practice/node-env-practice/udemy-2018-complete-web/robo-redux/src/components/CardList.js
+++ Practice/node-env-practice/udemy-2018-complete-web/robo-redux/src/components/CardList.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
-const CardList = ({robots}) => {
-    const cardComponent = robots.map((robot, i) => {
-        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    })
-    return(
-        <div>
-            {cardComponent}
-        </div>
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((robot, i) => {
+    return (
+      <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     );
-}
+  });
+  return <div>{cardComponent}</div>;
+};
 
 export default CardList;
