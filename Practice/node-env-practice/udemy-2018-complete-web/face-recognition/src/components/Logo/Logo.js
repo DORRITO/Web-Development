6d40a7f849a3ce226fb041544ae35e313e20b435
CHIp Wright <frisbeeprod@msn.com>
2018-05-19 15:09:29 -0700

    added tilt and log o

---
+++ Practice/node-env-practice/udemy-2018-complete-web/face-recognition/src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tilt from "react-tilt";
+
+const Logo = () => {
+  return (
+    <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }}>
+      <div className="Tilt-inner pa3">
+        <img style={{ paddingTop: "5px" }} alt="logo"  />
+      </div>
+    </Tilt>
+  );
+};
+
+export default Logo;
