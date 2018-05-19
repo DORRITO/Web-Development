77d824e4702b290310fc08bfd2867d382cb1b2a8
CHIp Wright <frisbeeprod@msn.com>
2018-05-19 15:39:50 -0700

    added particles

--- Practice/node-env-practice/udemy-2018-complete-web/face-recognition/src/App.js
+++ Practice/node-env-practice/udemy-2018-complete-web/face-recognition/src/App.js
@@ -3,12 +3,27 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
