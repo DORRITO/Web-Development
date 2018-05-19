9c32cde9ed5ea839f83d9056765fd593cddfa792
CHIp Wright <frisbeeprod@msn.com>
2018-05-18 19:34:13 -0700

    cleaned up some code

--- Practice/node-env-practice/udemy-2018-complete-web/star-wars/src/App.js
+++ Practice/node-env-practice/udemy-2018-complete-web/star-wars/src/App.js
@@ -1,32 +1,35 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      ship: ''
-    }
+      ship: ""
+    };
   }
-    
-  componentDidMount() { this.starWars(32) }
 
-  starWars = async (shipNum) => {
-    try{
-      const res = await fetch(`https://swapi.co/api/starships/${shipNum}`);
-      const ship = await res.json()
-      console.log(ship)
-      this.setState({ship: ship.name})
-    } catch(e){ console.log( "this isn't working", e)}
+  componentDidMount() {
+    this.starWars(32);
   }
-  
-  render(){
-    return(
+
+  starWars = async shipNum => {
+    try {
+      const res = await fetch(`https://swapi.co/api/starships/${shipNum}`);
+      const ship = await res.json();
+      this.setState({ ship: ship.name });
+    } catch(e) {
+      console.log("this isn't working", e);
+    }
+  };
+
+  render() {
+    return (
       <div>
         <h1>Star Wars Ships</h1>
         <h2>{this.state.ship}</h2>
       </div>
-    )
-  } 
-};
+    );
+  }
+}
 
 export default App;
