959dc1fb8a605f48a3d7b4013432c74008dc8a57
CHIp Wright <frisbeeprod@msn.com>
2018-05-19 12:57:41 -0700

    prac

--- Practice/node-env-practice/algo/AlgoCasts/exercises/fib/index.js
+++ Practice/node-env-practice/algo/AlgoCasts/exercises/fib/index.js
@@ -23,9 +23,8 @@
 // }
 
 function fib(n) {
-  // if(n < 2){return n}
-
-  // return fib(n - 1) + fib(n -2)
+  // if(n < 2) {return n}
+  // return fib(n -1) + fib(n -2)
 }
 
 // const fib = memoize(slowFib);
