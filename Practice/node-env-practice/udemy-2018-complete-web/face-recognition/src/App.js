8a6e76ed34abdec840fc755214fa1070cef0bd90
CHIp Wright <frisbeeprod@msn.com>
2018-05-19 18:13:36 -0700

    added clarifai

--- Practice/node-env-practice/udemy-2018-complete-web/face-recognition/src/App.js
+++ Practice/node-env-practice/udemy-2018-complete-web/face-recognition/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -6,6 +8,10 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 import Particles from 'react-particles-js';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'd9c603000d2c4dd6accd873b126b8bfd'
+})
 
 const particlesOptions = {
   particles: {
@@ -32,7 +38,14 @@ class App extends Component {
   }
 
   onButtonSubmit(){
-    console.log('click');
+    app.models.predict(Clarifai.COLOR_MODEL,
+      // URL
+      "https://samples.clarifai.com/metro-north.jpg")
+    .then(function(response) {
+      // do something with responseconsole.log(response);
+    },
+    function(err) {console.log(err)}
+    )
   }
 
   render() {
