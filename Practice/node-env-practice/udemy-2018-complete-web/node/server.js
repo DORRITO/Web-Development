3bc00cb5822fa450a6f884727632bb2a3448dfab
CHIp Wright <frisbeeprod@msn.com>
2018-05-22 00:55:53 -0700

    res and req examples

--- Practice/node-env-practice/udemy-2018-complete-web/node/server.js
+++ Practice/node-env-practice/udemy-2018-complete-web/node/server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log('I hear you! thanks for the request')
+const server = http.createServer((req, res) => {
+
+    const user = {
+        name: 'John',
+        hobby: 'skating'
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(user))
+    // res.setHeader('Content-Type', 'text/html');
+    // res.end('<h1>Hellooooooooooo</h1>')
 })
 
 server.listen(3000);
