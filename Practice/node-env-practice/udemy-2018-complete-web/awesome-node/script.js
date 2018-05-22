986b8956080b81268a4f39bee30fd8a456a1e567
CHIp Wright <frisbeeprod@msn.com>
2018-05-22 03:13:37 -0700

    read write append delete fs

--- Practice/node-env-practice/udemy-2018-complete-web/awesome-node/script.js
+++ Practice/node-env-practice/udemy-2018-complete-web/awesome-node/script.js
@@ -7,3 +7,19 @@ fs.readFile('./hello.txt', (err, data) => {
 
 const file = fs.readFileSync('./hello.txt');
 console.log('2', file.toString())
+
+//append
+// fs.appendFile('./hello.txt', 'this is so cool', err => { 
+//     if(err){console.log(err)}
+// })
+
+//write
+// fs.writeFile('./bye.txt', 'sad to see you go', err => { 
+//     if(err){console.log(err)}
+// })
+
+//delete
+// fs.unlink('./bye.txt', err => {
+//     if(err){console.log(err)}
+//     console.log('inception')
+// })
