e7974687454ca47a1b0b818dfc7e9e8392c5e34c
CHIp Wright <frisbeeprod@msn.com>
2018-05-23 01:53:24 -0700

    added image post route

--- Practice/node-env-practice/udemy-2018-complete-web/smartbrain-api/server.js
+++ Practice/node-env-practice/udemy-2018-complete-web/smartbrain-api/server.js
@@ -61,4 +61,17 @@ app.get('/profile/:id', (req, res) => {
     if(!found){res.status(400).json('not found')}
 })
 
+app.post('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach( user => {
+        if(user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(!found){res.status(400).json('not found')}
+})
+
 app.listen(3000, () => console.log("we are up and running on port 3000!"));
