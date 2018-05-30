de17c4af4d1c8fabedcbfd2faa20d19a59613b15
CHIp Wright <frisbeeprod@msn.com>
2018-05-30 00:04:43 -0700

    added genre exp to blues

--- Practice/reactv2-16-practice/second-violin/src/components/genres/Blues.js
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Blues.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SongPageList from "./../SongPageList";
+import GenreExp from "./../GenreExp";
 
 const Blues = () => {
   return (
     <div>
       Blues component
       <SongPageList />
+      <GenreExp />
     </div>
   );
 };
