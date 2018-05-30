d9ca48f332028f7e0a532ee45dacbb8c38cd25a3
CHIp Wright <frisbeeprod@msn.com>
2018-05-29 23:49:34 -0700

    turned rock comp into stateless

--- Practice/reactv2-16-practice/second-violin/src/components/genres/Rock.js
+++ Practice/reactv2-16-practice/second-violin/src/components/genres/Rock.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-import SongPageList from './../SongPageList';
+import SongPageList from "./../SongPageList";
 
-class Rock extends Component {
-  render() {
-    return(
-        <div>
-          Rock comp
-          <SongPageList />
-        </div>
-    )
-  }
-}
+const Rock = () => {
+  return (
+    <div>
+      Rock comp
+      <SongPageList />
+    </div>
+  );
+};
 
 export default Rock;
