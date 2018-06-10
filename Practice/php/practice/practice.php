28f897a52079ddfaac16759cc3ab6dc51632443d
CHIp Wright <frisbeeprod@msn.com>
2018-06-10 00:27:38 -0700

    arrays

--- Practice/php/practice/practice.php
+++ Practice/php/practice/practice.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +9,18 @@
 <body>
     This is working!
     <?php print "Hello again, <em>world</em>!"; ?>
-    <?php 
-        $hi = 'hi';
-        echo $hi; 
+    <?php $hi = 'hi'; echo $hi; ?>
+    <?php $arr = array("ok", "good", "then");
+        print $arr[0] . ' ' . $arr[2];
+    ?>
+    <br />
+    <?php
+        $arr2 = array(
+            "one" => 1,
+            "two" => 2,
+            "three" => 3
+        );
+        print $arr2["one"] . ' ' . $arr2["two"];
     ?>
 </body>
 </html>
