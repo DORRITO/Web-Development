3ff64b4d9af70e2b3a696582423c848cc91b6a75
CHIp Wright <frisbeeprod@msn.com>
2018-06-11 18:33:42 -0700

    do while loop added

--- Practice/php/practice/switch.php
+++ Practice/php/practice/switch.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>switch</title>
+    <title>switch/ do while</title>
 </head>
 <body>
     <?php
@@ -19,6 +19,12 @@
             default: 
                 print 'default';
         }
+    ?>
+
+    <?php 
+        do{
+            print '<br />' . 'hello!' . '<br />';
+        } while (1 > 2);
     ?>
 </body>
 </html>
