fb1e99d4bd2fad3b09f7f7683221390baa5fd383
CHIp Wright <frisbeeprod@msn.com>
2018-06-15 21:11:05 -0700

    added email script

---
+++ Practice/php/practice/email.php
@@ -0,0 +1,15 @@
+<php 
+    $to = "@yahoo.com";
+    $subject = "test test";
+    $msg = "short message";
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Contenet-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $headers .= 'To: @yahoo.com' . "\r\n";
+    $headers .= 'From: @yahoo.com' . "\r\n";
+    $headers .= 'Reply-To: @yahoo.com' . "\r\n";
+
+    $addParams = "-fwebmaster@anything.com";
+    mail($to, $subject, $msg, $headers, $addParams);
+?>
