d2fc865cde35f545873304d2d0c25f6cf2c8c561
CHIp Wright <frisbeeprod@msn.com>
2018-06-15 23:58:09 -0700

    comment

--- Practice/php/practice/email.php
+++ Practice/php/practice/email.php
@@ -5,11 +5,12 @@
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Contenet-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $headers .= 'To: @yahoo.com' . "\r\n";
+   
+    $headers .= 'To: @yahoo.com' . "\r\n"; 
     $headers .= 'From: @yahoo.com' . "\r\n";
     $headers .= 'Reply-To: @yahoo.com' . "\r\n";
 
     $addParams = "-fwebmaster@anything.com";
     mail($to, $subject, $msg, $headers, $addParams);
 ?>
+// add email to these
