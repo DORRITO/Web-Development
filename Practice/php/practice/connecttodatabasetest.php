264da38000bfe9913f92f1f4a467e35ab5af54c5
CHIp Wright <frisbeeprod@msn.com>
2018-07-02 23:40:06 -0700

    changing connect text

--- Practice/php/practice/connecttodatabasetest.php
+++ Practice/php/practice/connecttodatabasetest.php
@@ -1,32 +1,36 @@
 <?php
 
+function OpenCon(){
 $server = 'localhost';
 $username = 'drupaluser';
 $password = '';
 $database = 'drupal1';
 
 //connect
-$connection = mysqli_connect($server, $username, $password, $database);
+$connection = mysqli_connect($server, $username, $password, $database) or die("connect failed");
 
-//print out error if no connection
-if (!$connection){
-    die('<strong>unable to connect! error: , </strong>' . mysql_error() . '</strong>');
-};
+return $conn;
+}
+
 
 //start query
 mysql_select_db($database);
 
-//query
-$result = mysql_query("SELECT * FROM people");
-
-$output = '';
-while($row = mysql_fetch_array($result)){
-    foreach($row as $key => $val){
-        $output .=$key . ' = ' . $val . '<br />';
-    }
-    $output .= '<br />';
+function CloseCon($conn){
+    $conn -> close();
 }
 
-print $output;
-//temp not working//
+//query
+// $result = mysql_query("SELECT * FROM hi");
+
+// $output = '';
+// while($row = mysql_fetch_array($result)){
+//     foreach($row as $key => $val){
+//         $output .= $key . ' = ' . $val . '<br />';
+//     }
+//     $output .= '<br />';
+// }
+
+// print $output;
+
 ?>
