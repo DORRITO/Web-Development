336fae31c30efa69d140fd2e3b8e390cd27fd138
CHIp Wright <frisbeeprod@msn.com>
2018-07-31 03:06:44 -0700

    messing with bootstrap

--- Practice/reactv2-16-practice/nusura/src/App.js
+++ Practice/reactv2-16-practice/nusura/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Image } from "react-bootstrap";
+// import { Image } from "react-bootstrap";
 
 import Header from './components/Header';
 import Slogan from './components/Slogan';
@@ -10,11 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Image
-          className="Background1"
-          src="https://nusura.staging.wpengine.com/wp-content/uploads/2018/07/page-home-1.jpg"
-          responsive
-        />
         <Slogan />
       </div>
     );
--- Practice/reactv2-16-practice/nusura/src/components/Slogan.js
+++ Practice/reactv2-16-practice/nusura/src/components/Slogan.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
-import { Jumbotron } from "react-bootstrap";
+import { Image, Jumbotron } from "react-bootstrap";
 
 class Slogan extends Component {
   render() {
     return (
-      <Jumbotron className="Grey">
-        <div>UNIQUE EXPERIENCE</div>
-        <div>INNOVATIVE TECHNOLOGIES</div>
-        <h1>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h1>
-      </Jumbotron>
+      <div>
+        <Image
+          className="Background1"
+          src="https://nusura.staging.wpengine.com/wp-content/uploads/2018/07/page-home-1.jpg"
+          responsive
+        />
+        <Jumbotron className="Grey">
+          <div>UNIQUE EXPERIENCE</div>
+          <div>INNOVATIVE TECHNOLOGIES</div>
+          <h3>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h3>
+        </Jumbotron>
+      </div>
     );
   }
 }
