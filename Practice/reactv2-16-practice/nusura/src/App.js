c9e52581b9721d72069235f7d6bbbb2ea872394d
CHIp Wright <frisbeeprod@msn.com>
2018-07-31 02:30:02 -0700

    created slogan component

--- Practice/reactv2-16-practice/nusura/src/App.js
+++ Practice/reactv2-16-practice/nusura/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import {Image} from "react-bootstrap";
 import Header from './components/Header';
+import Slogan from './components/Slogan';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
         <Image src="https://nusura.staging.wpengine.com/wp-content/uploads/2018/07/page-home-1.jpg" responsive />;
+        <Slogan />
       </div>
     );
   }
---
+++ Practice/reactv2-16-practice/nusura/src/components/Slogan.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Slogan extends Component{
+    render(){
+        return(
+            <div className="Grey">
+                <div>UNIQUE EXPERIENCE</div>
+                <div>INNOVATIVE TECHNOLOGIES</div>
+                <h1>BUILDING A BETTER, SAFER, MORE RESILIENT FUTURE</h1>
+            </div>
+        )
+    }
+}
+
+export default Slogan;
