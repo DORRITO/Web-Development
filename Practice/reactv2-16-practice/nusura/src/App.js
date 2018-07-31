8eb2c72685f58d3a04c9cf418dd24fecc54fa643
CHIp Wright <frisbeeprod@msn.com>
2018-07-31 02:11:45 -0700

    added bootstrap img to make the page responsive

--- Practice/reactv2-16-practice/nusura/src/App.js
+++ Practice/reactv2-16-practice/nusura/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Image} from "react-bootstrap";
 import Header from './components/Header';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header></Header>
-        <img className="Background1" src="https://nusura.staging.wpengine.com/wp-content/uploads/2018/07/page-home-1.jpg"></img>
+        <Image src="https://nusura.staging.wpengine.com/wp-content/uploads/2018/07/page-home-1.jpg" responsive />;
       </div>
     );
   }
