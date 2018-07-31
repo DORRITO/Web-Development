3acce2c8e98ccb1c0e16b7779d007dd764966e02
CHIp Wright <frisbeeprod@msn.com>
2018-07-30 22:48:43 -0700

    moved files around, added bootstrap calls to header

--- Practice/reactv2-16-practice/nusura/src/App.js
+++ Practice/reactv2-16-practice/nusura/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 
@@ -7,14 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={'https://nusura.com/wp-content/uploads/2018/06/n_rev_small3.png'} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Nusura</h1>
-        </header>
         <Header></Header>
-        <p className="App-intro">
-          Start here
-        </p>
       </div>
     );
   }
--- Practice/reactv2-16-practice/nusura/src/components/Header.js
+++ Practice/reactv2-16-practice/nusura/src/components/Header.js
@@ -1,13 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 
 class Header extends Component {
-    render(){
-        return(
-            <div>
-                Hello this is the header
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">
+              <img src={"https://nusura.com/wp-content/uploads/2018/06/n_rev_small3.png"}
+              className="App-logo"
+              alt="logo"
+            />
+              </a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+              <NavDropdown
+                eventKey={3}
+                title="Dropdown"
+                id="basic-nav-dropdown"
+              >
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>;
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Header;
