076b7d5a0e2d626350a6a54a1add01fcfd209b69
CHIp Wright <frisbeeprod@msn.com>
2018-07-30 23:26:20 -0700

    moved links to the right

--- Practice/reactv2-16-practice/nusura/src/components/Header.js
+++ Practice/reactv2-16-practice/nusura/src/components/Header.js
@@ -5,29 +5,28 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse collapseOnSelect fixedTop>
+        About Products & Services SimulationDeck Case Studies News Contact
+        <Navbar className="Navbar" inverse collapseOnSelect staticTop>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#brand">
-              <img src={"https://nusura.com/wp-content/uploads/2018/06/n_rev_small3.png"}
-              className="App-logo"
-              alt="logo"
-            />
+              <a href="https://nusura.com" target="_blank">
+                <img
+                  src={
+                    "https://nusura.com/wp-content/uploads/2018/06/n_rev_small3.png"
+                  }
+                  className="App-logo"
+                  alt="logo"
+                />
               </a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#">
-                Link
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link
-              </NavItem>
+            <Nav />
+            <Nav pullRight>
               <NavDropdown
-                eventKey={3}
-                title="Dropdown"
+                eventKey={1}
+                title="About"
                 id="basic-nav-dropdown"
               >
                 <MenuItem eventKey={3.1}>Action</MenuItem>
@@ -36,20 +35,25 @@ class Header extends Component {
                 <MenuItem divider />
                 <MenuItem eventKey={3.3}>Separated link</MenuItem>
               </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">
+              <NavItem eventKey={2} href="#">
+                Producs & Services
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                SimulationDeck
+              </NavItem>
+              <NavItem eventKey={4} href="#">
                 Link Right
               </NavItem>
-              <NavItem eventKey={2} href="#">
+              <NavItem eventKey={5} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={6} href="#">
                 Link Right
               </NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>;
-        <header className="App-header">
-          
-        </header>
+        <header className="App-header" />
       </div>
     );
   }
